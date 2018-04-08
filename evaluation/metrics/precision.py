b3737b13f377768e4ffee3f820ce6d06e7ffa5b7
Alexander Petrov <firexel@gmail.com>
2018-04-08 18:42:45 +0100

    metric is calculated by actual actions

--- evaluation/metrics/precision.py
+++ evaluation/metrics/precision.py
@@ -1,5 +1,5 @@
-def precision(recommendations, actual_items, k):
-    actual_set = set(actual_items)
+def precision(recommendations, actual_actions, k):
+    actual_set = set([action.item_id for action in actual_actions])
     recommended = set([recommendation[0] for recommendation in recommendations[:k]])
     cool = recommended.intersection(actual_set)
     return len(cool) / len(recommended)
--- tests/test_precision.py
+++ tests/test_precision.py
@@ -1,10 +1,12 @@
 from aprec.evaluation.metrics.precision import precision
+from aprec.api.action import Action
 import unittest
 
 class TestPrecision(unittest.TestCase):
     def test_precsion(self):
         recommended = [(1, 2), (2, 1), (3, 0.5)]
-        actual = [1, 3]
+        actual = [Action(user_id = 1, item_id = 1, timestamp=1), 
+                  Action(user_id = 1, item_id = 3, timestamp=2)]
         self.assertEqual(precision(recommended, actual, 1), 1)
         self.assertEqual(precision(recommended, actual, 2), 0.5)
         self.assertEqual(precision(recommended, actual, 3), 2/3)
