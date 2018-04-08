f05aaffe89eb501da75ff758b9d2f84658d1f967
Alexander Petrov <firexel@gmail.com>
2018-04-08 21:41:48 +0100

    measure ndcg on full dataset

--- evaluation/sample_config.py
+++ evaluation/sample_config.py
@@ -3,13 +3,14 @@ from aprec.recommenders.top_recommender import TopRecommender
 from aprec.utils import generator_limit
 from aprec.evaluation.metrics.precision import Precision
 from aprec.evaluation.metrics.recall import Recall
+from aprec.evaluation.metrics.ndcg import NDCG
 
 
-DATASET = generator_limit(get_movielens_actions(min_rating=4.0), 100000)
+DATASET = generator_limit(get_movielens_actions(min_rating=4.0), 1000000)
 RECOMMENDERS = {
     "top_recommender": lambda: TopRecommender()    
 }
 
 FRACTIONS_TO_SPLIT = (0.2, 0.4, 0.6, 0.8)
 TEST_ACTION_PER_USER = 1
-METRICS = [Precision(1), Recall(1), Precision(5), Recall(5)]
+METRICS = [Precision(5), NDCG(50)]
