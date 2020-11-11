fixed bug in normalization

recommenders/losses/lambdarank.py
@@ -1,32 +1,32 @@
-import math
-
-from tensorflow.keras import backend as K
 import tensorflow as tf
+import tensorflow.keras.backend as K
 
-
-def get_pairwise_diff_vector(x):
-    a, b = tf.meshgrid(x, tf.transpose(x))
-    return tf.subtract(b, a)
-
-def get_pairwise_diff_batch(x):
-    return tf.map_fn(get_pairwise_diff_vector, x)
-
-def need_swap_vector(x):
-    diffs = tf.cast(tf.sign(get_pairwise_diff_vector(x)), tf.float32)
-    return diffs
-
-def need_swap_batch(x):
-    return tf.map_fn(need_swap_vector, x)
-
+def print_tensor(name, tensor):
+    print(name)
+    K.print_tensor(tensor)
 
 class PairwiseLoss(object):
+    def get_pairwise_diff_vector(self, x):
+        a, b = tf.meshgrid(x, tf.transpose(x))
+        return tf.subtract(b, a)
+
+    def get_pairwise_diff_batch(self, x):
+        return tf.map_fn(self.get_pairwise_diff_vector, x)
+
+    def need_swap_vector(self, x):
+        diffs = tf.cast(tf.sign(self.get_pairwise_diff_vector(x)), tf.float32)
+        return diffs
+
+    def need_swap_batch(self, x):
+        return tf.map_fn(self.need_swap_vector, x)
+
     def __init__(self, k, batch_size, sigma=0.5):
         self.__name__ = 'pairwise_loss'
         self.batch_size = batch_size
         self.sigma = sigma
         self.k = k
         self.dcg_position_discounts = tf.math.log(2.0) / tf.math.log(tf.cast(tf.range(k) + 2, tf.float32))
-        self.swap_importance = tf.abs(get_pairwise_diff_vector(self.dcg_position_discounts))
+        self.swap_importance = tf.abs(self.get_pairwise_diff_vector(self.dcg_position_discounts))
 
     def dcg(self, x):
         return tf.reduce_sum((2 ** x - 1) * self.dcg_position_discounts)
@@ -55,21 +55,20 @@ class PairwiseLoss(object):
 
         true_ordered = tf.gather(y_true, top_k.indices, batch_dims=1)
         inverse_idcg = tf.reshape(tf.map_fn(self.inverse_idcg, true_ordered), (self.batch_size, 1, 1))
-        #K.print_tensor(inverse_idcg)
-        S = need_swap_batch(true_ordered)
+        S = self.need_swap_batch(true_ordered)
         true_gains = 2 ** true_ordered - 1
-        true_gains_diff = get_pairwise_diff_batch(true_gains)
+        true_gains_diff = self.get_pairwise_diff_batch(true_gains)
         abs_delta_ndcg = tf.abs(true_gains_diff * self.swap_importance) * inverse_idcg
-        #K.print_tensor(self.swap_importance)
-        pairwise_diffs = get_pairwise_diff_batch(pred_ordered) * S
+        pairwise_diffs = self.get_pairwise_diff_batch(pred_ordered) * S
 
         norms = (1 - range_is_zero) * (tf.abs(pairwise_diffs) + 0.01) + (range_is_zero)
         abs_delta_ndcg = abs_delta_ndcg / norms
         sigmoid = 1.0 / (1 + tf.exp(self.sigma * (pairwise_diffs)))
+
         #ranknet lambdas
         #lambda_matrix = self.sigma * (0.5 * (1 - S) -sigmoid)
         lambda_matrix = -self.sigma * abs_delta_ndcg * sigmoid * S
-        all_lambdas_sum = tf.reduce_sum(tf.abs(lambda_matrix))
+        all_lambdas_sum = tf.reshape(tf.reduce_sum(tf.abs(lambda_matrix), axis=(1,2)), (self.batch_size, 1))
 
         norm_factor = tf.math.log(all_lambdas_sum + 1) / (all_lambdas_sum * tf.math.log(2.0))
         lambda_sum = tf.reduce_sum(lambda_matrix, axis=2) * norm_factor
@@ -80,33 +79,3 @@ class PairwiseLoss(object):
         result_lambdas = tf.scatter_nd(indices, tf.reshape(lambda_sum,[self.k*self.batch_size]),
                                        tf.constant([self.batch_size, self.k]))
         return result_lambdas
-
-
-class LambdaRankLoss(object):
-    def __init__(self, k=20, eps=1e-6):
-        self.eps = eps
-        self.k = k
-        self.__name__ = f"lambdarank_{k}"
-        discount = []
-        for i in range(1, k + 1):
-            discount.append(1 / math.log2(i + 1))
-        self.discount = K.constant(discount)
-
-    def __call__(self, y_true, y_pred):
-        z = get_pairwise_diff_batch(y_pred)
-        #K.print_tensor(z)
-        return self.get_res_pred_discounted(y_true, y_pred) + self.get_res_true_discounted(y_true, y_pred)
-
-    def get_res_pred_discounted(self, y_true, y_pred):
-        top_k = tf.nn.top_k(y_pred, self.k)
-        true_vals = tf.gather(y_true, top_k.indices, batch_dims=1)
-        positive_log = K.log(top_k.values + self.eps)
-        negative_log = K.log(1 - top_k.values + self.eps)
-        return K.mean((-true_vals * positive_log - (1 - true_vals) * negative_log) * self.discount)
-
-    def get_res_true_discounted(self, y_true, y_pred):
-        top_k = tf.nn.top_k(y_true, self.k)
-        chired_vals = tf.gather(y_pred, top_k.indices, batch_dims=1)
-        positive_log = K.log(pred_vals + self.eps)
-        negative_log = K.log(1 - pred_vals + self.eps)
-        return K.mean((-top_k.values * positive_log - (1 - top_k.values) * negative_log) * self.discount)

tests/test_lambdarank_loss.py
@@ -1,73 +1,34 @@
-from aprec.recommenders.losses.lambdarank import LambdaRankLoss, get_pairwise_diff_batch, need_swap_vector, PairwiseLoss
-from tensorflow.keras.losses import binary_crossentropy
+from aprec.recommenders.losses.lambdarank import  PairwiseLoss
 import tensorflow.keras.backend as K
 from tensorflow.keras import Sequential
-from tensorflow.keras.layers import Dense, Input
-import numpy as np
+from tensorflow.keras.layers import Dense
+import tensorflow as tf
 import unittest
 
-from tensorflow.python.keras.optimizers import SGD
-
-
 class TestLambdarankLoss(unittest.TestCase):
-    def test_lambdarank_loss(self):
-        return
-        y_true = K.constant(np.array([[0, 1, 0], [1, 1, 0]]))
-        y_pred = K.constant(np.array([[0.1, 0.3, 0.2], [0.4, 0.5, 0.6]]))
-        loss = LambdaRankLoss(3)
-        loss_val = loss(y_true, y_pred)
-        ce_loss = binary_crossentropy(y_true, y_pred)
-        #K.print_tensor(ce_loss)
-        #K.print_tensor(loss_val)
-
-    def test_pairwise_diff(self):
-        loss = LambdaRankLoss(1000)
-        x = K.constant([[1, 2, 3], [1, 1, 1]])
-        z = get_pairwise_diff_batch(x)
-        K.print_tensor(z)
-
-    def test_pairwise_diff(self):
-        x = K.constant([[1, 2, 3], [1, 1, 1], [0.1, 0.2, 0.3]])
-        z = get_pairwise_diff_batch(x)
-        K.print_tensor(z)
-
-    def test_need_swap(self):
-        x = K.constant([1, 0, 0.5, 1])
-        z = need_swap_vector(x)
-        K.print_tensor(z)
+    def test_lambdas_sample(self, y, s, lambdas):
+        y_true = K.constant(y)
+        y_pred = K.constant(s)
+        expected_lambdas = lambdas
+        loss = PairwiseLoss(len(y_true[0]), len(y_true), 1)
+        lambdas = loss.get_lambdas(y_true, y_pred)
+        K.print_tensor(lambdas)
+        eps = 1e-4
+        res = tf.reduce_sum(tf.abs(lambdas - expected_lambdas))
+        assert (res < eps)
 
     def test_get_lambdas(self):
-        y_true = K.constant([[0, 0, 1, 0]])
-        y_pred = K.constant([[1, 0, 1, 0]])
-        loss = PairwiseLoss(len(y_true[0]), len(y_true), 1)
-        current = y_pred
-        lr = 0.1
-        for i in range(1):
-            lambdas = loss.get_lambdas(y_true, current)
-            current =  current - lr * lambdas
-            print(f"iter: {i}")
-            print("CURRENT")
-            K.print_tensor(current)
-            print("LAMBDA")
-            K.print_tensor(lambdas)
-            print("XXX")
+        self.test_lambdas_sample([[0, 0, 1, 0], [0, 0, 1, 0]],
+                                 [[0.1, 0.3, 1, 0], [0.5, 0, 0.5, 0]],
+                                 [[0.160353, 0.174145, -0.487562, 0.153063], [2.59696, 0.0136405, -2.63147, 0.0208627]])
+        self.test_lambdas_sample([[0, 0, 1, 0]], [[0.1, 0.3, 1, 0]], [[0.160353, 0.174145, -0.487562, 0.153063]])
+        self.test_lambdas_sample([[0, 0, 1, 0]],[[0.5, 0, 0.5, 0]], [[2.59696, 0.0136405, -2.63147, 0.0208627]])
+
 
     def test_dcg(self):
         loss = PairwiseLoss(4, 2)
-        res = loss.idcg(K.constant([0, 0, 0, 1]))
-        K.print_tensor(res)
-
-    def test_divide(self):
-        import tensorflow as tf
-        a = tf.constant([
-            [1, 2, 3, 4],
-            [5, 6, 7, 8]
-        ], dtype=tf.float32)
-        b = tf.reduce_sum(a, axis=1)
-        z = tf.reshape(b, (-1, 1))
-        K.print_tensor(z)
-        c = a / tf.reshape(b, (-1, 1))
-        K.print_tensor(c)
+        res = loss.inverse_idcg(K.constant([0, 0, 0, 1]))
+        assert res == 1
 
     def test_model_lambdarank(self):
         model = Sequential()
@@ -76,12 +37,12 @@ class TestLambdarankLoss(unittest.TestCase):
         model.add(Dense(2, activation='sigmoid'))
         loss = PairwiseLoss(2, 2, sigma=1)
         model.compile(optimizer='adam', loss=loss)
-
         X = K.constant([[0, 0], [1, 0]])
         Y = K.constant([[1, 0],  [0, 1]])
-        model.fit(X, Y, epochs=1000)
+        model.fit(X, Y, epochs=1000,verbose=False)
         result = model.predict(X)
-        K.print_tensor(result)
+        assert(result[0,0] > result [0, 1])
+        assert(result[1,0] < result [1, 1])
 
 if __name__ == "__main__":
     unittest.main()