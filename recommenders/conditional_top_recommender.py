7902c7dfa32578a4ae561f2108d8300ef7c0c15f
Yuriy Makarov <yury.makarov@corp.mail.ru>
2021-01-04 18:25:20 +0300

    first commit

---
+++ recommenders/conditional_top_recommender.py
@@ -0,0 +1,35 @@
+from collections import Counter
+from aprec.recommenders.recommender import Recommender
+
+
+class ConditionalTopRecommender(Recommender):
+    def __init__(self, conditional_field: str):
+        self.conditional_field: str = conditional_field
+        self.items_counts: dict = dict()
+        self.user_field_values: dict = dict()
+
+    def add_action(self, action):
+        if self.conditional_field not in action.data:
+            raise Exception(f"this actions does not have required field: {self.conditional_field}")
+
+        field_value: str = action.data[self.conditional_field]
+        if field_value not in self.items_counts:
+            self.items_counts[field_value] = Counter()
+        self.user_field_values[action.user_id] = field_value
+
+        if action.item_id is not None:
+            self.items_counts[field_value][action.item_id] += 1
+
+    def rebuild_model(self):
+        pass
+
+    def get_next_items(self, user_id, limit):
+        if user_id not in self.user_field_values:
+            raise Exception("New user without field value")
+        return self.items_counts[self.user_field_values[user_id]].most_common()[:limit]
+
+    def get_similar_items(self, item_id, limit):
+        raise NotImplementedError
+
+    def name(self):
+        return "ConditionalTopItemsRecommender"
