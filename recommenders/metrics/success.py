fixed success metrics, added batch normalization after almost every layer in the model

recommenders/metrics/success.py
@@ -9,5 +9,6 @@ class KerasSuccess(object):
    def __call__(self, y_true, y_pred):
         top_k = tf.nn.top_k(y_pred, self.k)
         gains = tf.gather(y_true, top_k.indices, batch_dims=1)
-        return  K.mean(gains)
+        user_success = K.sum(gains, axis=-1)
+        return K.mean(user_success)
 