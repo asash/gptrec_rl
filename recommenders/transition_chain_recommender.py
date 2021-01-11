361bb05650f68e2be0f48c4e831016f9267332f8
Yuriy Makarov <yury.makarov@corp.mail.ru>
2021-01-11 21:29:10 +0300

    updated transition_chain_recommender.py

--- recommenders/transition_chain_recommender.py
+++ recommenders/transition_chain_recommender.py
@@ -7,52 +7,39 @@ class TransitionsChainRecommender(Recommender):
     """
     TODO
     """
-    def __init__(self, chain_indicator_field: str):
+    def __init__(self):
         self.item_id_to_index: dict = dict()
         self.index_to_item_id: dict = dict()
         self.items_count: int = 0
         self.transition_matrix: np.array = np.array([])
 
-        self.user_field_values: dict = dict()
-        self.chain_to_items: dict = dict()
-        self.chain_indicator_field: str = chain_indicator_field
+        self.user_to_items: dict = dict()
 
     def add_action(self, action):
-        if self.chain_indicator_field not in action.data:
-            raise Exception(f"this actions does not have required field: {self.chain_indicator_field}. Contains: {action.data}")
-
         if action.item_id not in self.item_id_to_index:
             self.item_id_to_index[action.item_id] = self.items_count
             self.index_to_item_id[self.items_count] = action.item_id
             self.items_count += 1
 
-        if action.data[self.chain_indicator_field] not in self.chain_to_items:
-            self.chain_to_items[action.data[self.chain_indicator_field]] = [action.item_id]
+        if action.user_id not in self.user_to_items:
+            self.user_to_items[action.user_id] = [action.item_id]
         else:
-            self.chain_to_items[action.data[self.chain_indicator_field]].append(action.item_id)
-
-        if action.user_id not in self.user_field_values:
-            self.user_field_values[action.user_id] = [action.data[self.chain_indicator_field]]
-        else:
-            if action.data[self.chain_indicator_field] not in self.user_field_values[action.user_id]:
-                self.user_field_values[action.user_id].append(action.data[self.chain_indicator_field])
+            self.user_to_items[action.user_id].append(action.item_id)
 
     def rebuild_model(self):
         self.transition_matrix = np.zeros(shape=(self.items_count, self.items_count))
-        # считать попарно, а не только в таргет?
-        for _, items in self.chain_to_items.items():
+        for _, items in self.user_to_items.items():
             target_item = items[-1]
             for item_id in items[:-1]:
                 self.transition_matrix[self.item_id_to_index[item_id]][self.item_id_to_index[target_item]] += 1
 
     def get_next_items(self, user_id, limit):
-        if user_id not in self.user_field_values:
+        if user_id not in self.user_to_items:
             raise Exception("New user without field value")
-        last_chain_id = self.user_field_values[user_id][-1]
-        last_chain_items_indexes = [self.index_to_item_id[idx] for idx in self.chain_to_items[last_chain_id]]
+        last_items_indexes = [self.item_id_to_index[idx] for idx in self.user_to_items[user_id]]
         total_predictions = [
             (index, prob) for index, prob in zip(
-                range(self.items_count), np.sum(self.transition_matrix[last_chain_items_indexes], axis=0)
+                range(self.items_count), np.sum(self.transition_matrix[last_items_indexes], axis=0)
             )
         ]
         total_predictions.sort(key=lambda x: -x[1])
--- tests/test_transition_chain_recommender.py
+++ tests/test_transition_chain_recommender.py
@@ -7,25 +7,25 @@ from aprec.recommenders.transition_chain_recommender import TransitionsChainReco
 
 class TestTransitionsChainRecommender(unittest.TestCase):
     def test_transitions_chain_recommender(self):
-        recommender = TransitionsChainRecommender(chain_indicator_field='utrip_id')
+        recommender = TransitionsChainRecommender()
         actions: List[Action] = [
             Action(user_id=0, item_id=0, timestamp=0, data={'utrip_id': 100}),
             Action(user_id=0, item_id=1, timestamp=10, data={'utrip_id': 100}),
             Action(user_id=0, item_id=2, timestamp=20, data={'utrip_id': 100}),
 
-            Action(user_id=0, item_id=3, timestamp=0, data={'utrip_id': 200}),
-            Action(user_id=0, item_id=1, timestamp=10, data={'utrip_id': 200}),
-            Action(user_id=0, item_id=2, timestamp=20, data={'utrip_id': 200}),
+            Action(user_id=2, item_id=3, timestamp=0, data={'utrip_id': 200}),
+            Action(user_id=2, item_id=1, timestamp=10, data={'utrip_id': 200}),
+            Action(user_id=2, item_id=2, timestamp=20, data={'utrip_id': 200}),
 
-            Action(user_id=0, item_id=1, timestamp=10, data={'utrip_id': 2000}),
-            Action(user_id=0, item_id=3, timestamp=20, data={'utrip_id': 2000}),
+            Action(user_id=3, item_id=1, timestamp=10, data={'utrip_id': 2000}),
+            Action(user_id=3, item_id=3, timestamp=20, data={'utrip_id': 2000}),
 
-            Action(user_id=0, item_id=1, timestamp=0, data={'utrip_id': 300}),
+            Action(user_id=4, item_id=1, timestamp=0, data={'utrip_id': 300}),
         ]
         for action in actions:
             recommender.add_action(action)
         recommender.rebuild_model()
-        recommendations = recommender.get_next_items(0, 2)
+        recommendations = recommender.get_next_items(4, 2)
         self.assertEqual(recommendations, [(2, 2), (3, 1)])
 
 if __name__ == "__main__":
