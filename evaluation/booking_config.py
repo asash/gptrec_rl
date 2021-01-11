d8650762e5d1897e090e03c190ac13a6adb7c6d8
Aleksandr Petrov <firexel@gmail.com>
2021-01-11 21:04:27 +0000

    added multiple versions of the embedding size to prepare full-scale experiment with selfattention

--- evaluation/booking_config.py
+++ evaluation/booking_config.py
@@ -29,7 +29,7 @@ def svd_recommender(k):
 def item_item_recommender():
     return ItemItemRecommender()
 
-def mlp_historical_embedding(loss, activation_override=None):
+def mlp_historical_embedding(loss, activation_override=None, bottleneck_size=64):
     activation = 'linear' if loss == 'lambdarank' else 'sigmoid'
     if activation_override is not None:
         activation = activation_override
@@ -37,7 +37,7 @@ def mlp_historical_embedding(loss, activation_override=None):
                                         optimizer=Adam(), early_stop_epochs=20,
                                         batch_size=250, sigma=1.0, ndcg_at=40,
                                         n_val_users=2000,
-                                        bottleneck_size=64,
+                                        bottleneck_size=bottleneck_size,
                                         max_history_len=50,
                                         output_layer_activation=activation)
 
@@ -47,12 +47,17 @@ RECOMMENDERS = {
     "svd_recommender": lambda: svd_recommender(30),
     "item_temem_recommender": item_item_recommender,
     "transitions_chain_recommender": TransitionsChainRecommender,
-    "APREC-GMLPHE-Lambdarank": lambda: mlp_historical_embedding('lambdarank', 'linear'),
+    "APREC-GMLPHE-Lambdarank-32": lambda: mlp_historical_embedding('lambdarank', 'linear', 32),
+    "APREC-GMLPHE-Lambdarank-64": lambda: mlp_historical_embedding('lambdarank', 'linear', 64),
+    "APREC-GMLPHE-Lambdarank-128": lambda: mlp_historical_embedding('lambdarank', 'linear', 128),
+    "APREC-GMLPHE-Lambdarank-256": lambda: mlp_historical_embedding('lambdarank', 'linear', 256),
+    "APREC-GMLPHE-Lambdarank-512": lambda: mlp_historical_embedding('lambdarank', 'linear', 512),
+    "APREC-GMLPHE-Lambdarank-1024": lambda: mlp_historical_embedding('lambdarank', 'linear', 1024),
 }
 
 SPLIT_STRATEGY = "LEAVE_ONE_OUT"
 
-USERS_FRACTIONS = [0.1]
+USERS_FRACTIONS = [1.0]
 
 dataset_for_metric = [action for action in get_booking_dataset('./booking_data/booking_train_set.csv')]
 METRICS = [Precision(4), SPS(4), NDCG(4), NDCG(40)]
