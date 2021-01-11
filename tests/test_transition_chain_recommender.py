61acb8281ba66b6991218e32a6f5ea6e5801e70d
Yuriy Makarov <yury.makarov@corp.mail.ru>
2021-01-11 20:02:36 +0300

    transition chain prototype and test

---
+++ tests/test_transition_chain_recommender.py
@@ -0,0 +1,32 @@
+import unittest
+
+from typing import List
+from aprec.api.action import Action
+from aprec.recommenders.transition_chain_recommender import TransitionsChainRecommender
+
+
+class TestTransitionsChainRecommender(unittest.TestCase):
+    def test_transitions_chain_recommender(self):
+        recommender = TransitionsChainRecommender(chain_indicator_field='utrip_id')
+        actions: List[Action] = [
+            Action(user_id=0, item_id=0, timestamp=0, data={'utrip_id': 100}),
+            Action(user_id=0, item_id=1, timestamp=10, data={'utrip_id': 100}),
+            Action(user_id=0, item_id=2, timestamp=20, data={'utrip_id': 100}),
+
+            Action(user_id=0, item_id=3, timestamp=0, data={'utrip_id': 200}),
+            Action(user_id=0, item_id=1, timestamp=10, data={'utrip_id': 200}),
+            Action(user_id=0, item_id=2, timestamp=20, data={'utrip_id': 200}),
+
+            Action(user_id=0, item_id=1, timestamp=10, data={'utrip_id': 2000}),
+            Action(user_id=0, item_id=3, timestamp=20, data={'utrip_id': 2000}),
+
+            Action(user_id=0, item_id=1, timestamp=0, data={'utrip_id': 300}),
+        ]
+        for action in actions:
+            recommender.add_action(action)
+        recommender.rebuild_model()
+        recommendations = recommender.get_next_items(0, 2)
+        self.assertEqual(recommendations, [(2, 2), (3, 1)])
+
+if __name__ == "__main__":
+    unittest.main()
