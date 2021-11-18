3a3dceef9b00d47d77fc10c3a7546f59b218e488
Aleksandr Petrov <firexel@gmail.com>
2021-11-18 17:56:49 +0000

    config for salrec hyperparameters search

--- evaluation/analyze_experiment_in_progress.py
+++ evaluation/analyze_experiment_in_progress.py
@@ -7,7 +7,7 @@ import sys
 
 pd.set_option("display.max_rows", None, "display.max_columns", None)
 pd.set_option('display.expand_frame_repr', False) 
-pd.set_option('display.max_colwidth', 100) 
+pd.set_option('display.max_colwidth', 256) 
 
 
 
