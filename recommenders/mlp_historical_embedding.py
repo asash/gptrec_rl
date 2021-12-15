7830500dec7ffddc882e59391d248fca228c60ae
Aleksandr Petrov <firexel@gmail.com>
2021-12-15 17:22:55 +0000

    added GRU4Rec support in the mlp_historical_embedding.py

--- recommenders/mlp_historical_embedding.py
+++ recommenders/mlp_historical_embedding.py
@@ -1,19 +1,19 @@
 import gc
 import time
 
+
+from aprec.losses.get_loss import get_loss
 import tensorflow.keras.backend as K
 from collections import defaultdict
 
-from aprec.losses.bpr import BPRLoss
 from aprec.utils.item_id import ItemId
 from aprec.recommenders.metrics.ndcg import KerasNDCG
-from aprec.losses.lambdarank import LambdaRankLoss
-from aprec.losses.xendcg import XENDCGLoss
+from aprec.losses.lambdarank import  LambdarankLambdasSum, BCELambdasSum
 from aprec.recommenders.recommender import Recommender
 from aprec.recommenders.history_batch_generator import HistoryBatchGenerator
 from aprec \
     .recommenders.history_batch_generator import actions_to_vector
-from tensorflow.keras.models import Sequential
+from tensorflow.keras.models import Sequential, Model
 import tensorflow.keras.layers as layers
 import numpy as np
 
@@ -29,7 +29,18 @@ class GreedyMLPHistoricalEmbedding(Recommender):
                  target_decay = 1.0,
                  sigma=1,
                  ndcg_at=30,
-                 ):
+                 model_arch='mlp_embedding',
+                 loss_internal_dtype='float32',
+                 loss_lambda_normalization = True,
+                 training_time_limit = None,
+                 loss_pred_truncate=2500,
+                 loss_bce_weight = 0.975,
+                 embedding_size = 64,
+                 num_main_layers = 3,
+                 num_dense_layers = 1,
+                 log_lambdas_len = False,
+                 eval_ndcg_at=None,
+    ):
         self.users = ItemId()
         self.items = ItemId()
         self.user_actions = defaultdict(lambda: [])
@@ -50,6 +61,21 @@ class GreedyMLPHistoricalEmbedding(Recommender):
         self.output_layer_activation = output_layer_activation
         self.target_decay = target_decay
         self.val_users = None
+        self.model_arch = model_arch
+        self.loss_internal_dtype = loss_internal_dtype
+        self.loss_lambda_normalization = loss_lambda_normalization
+        self.loss_pred_truncate = loss_pred_truncate
+        self.loss_bce_weight = loss_bce_weight
+        self.embedding_size = embedding_size
+        self.num_main_layers = num_main_layers
+        self.num_dense_layers = num_dense_layers
+        self.eval_ndcg_at=40
+        self.training_time_limit = training_time_limit
+        self.eval_ndcg_at = eval_ndcg_at if eval_ndcg_at is not None else self.ndcg_at
+
+        if log_lambdas_len and loss != 'lambdarank':
+            raise Exception("logging lambdas len is only possible with lambdarank loss")
+        self.log_lambdas_len = log_lambdas_len
 
     def get_metadata(self):
         return self.metadata
@@ -83,7 +109,7 @@ class GreedyMLPHistoricalEmbedding(Recommender):
         val_generator = HistoryBatchGenerator(val_users, self.max_history_length, self.items.size(),
                                               batch_size=self.batch_size, validation=True,
                                               target_decay=self.target_decay)
-        self.model = self.get_model(self.items.size())
+        self.model = self.get_model()
         best_ndcg = 0
         steps_since_improved = 0
         best_epoch = -1
@@ -114,6 +140,11 @@ class GreedyMLPHistoricalEmbedding(Recommender):
             if steps_since_improved >= self.early_stop_epochs:
                 print(f"early stopped at epoch {epoch}")
                 break
+
+            if self.training_time_limit is not None and total_trainig_time > self.training_time_limit:
+                print(f"time limit stop triggered at epoch {epoch}")
+                break
+
             K.clear_session()
             gc.collect()
         self.model.set_weights(best_weights)
@@ -130,9 +161,10 @@ class GreedyMLPHistoricalEmbedding(Recommender):
         train_users = self.user_actions_by_id_list(train_user_ids)
         return train_users, val_users
 
-    def get_model(self, n_movies):
+
+    def get_mlp_model(self):
         model = Sequential(name='MLP')
-        model.add(layers.Embedding(n_movies + 1, 32, input_length=self.max_history_length))
+        model.add(layers.Embedding(self.items.size() + 1, 32, input_length=self.max_history_length))
         model.add(layers.Flatten())
         model.add(layers.Dense(256, name="dense1", activation="relu"))
         model.add(layers.Dense(128, name="dense2", activation="relu"))
@@ -141,29 +173,47 @@ class GreedyMLPHistoricalEmbedding(Recommender):
         model.add(layers.Dropout(0.5, name="dropout"))
         model.add(layers.Dense(128, name="dense3", activation="relu"))
         model.add(layers.Dense(256, name="dense4", activation="relu"))
-        model.add(layers.Dense(n_movies, name="output", activation=self.output_layer_activation))
-        ndcg_metric = KerasNDCG(self.ndcg_at)
-        loss = self.loss
-        if loss == 'lambdarank':
-            loss = self.get_lambdarank_loss()
+        model.add(layers.Dense(self.items, name="output", activation=self.output_layer_activation))
 
-        if loss == 'xendcg':
-            loss = self.get_xendcg_loss()
 
-        if loss == 'bpr':
-            loss = self.get_bpr_loss()
+    def get_gru_model(self):
+
+        input = layers.Input(shape=(self.max_history_length))
+        x = layers.Embedding(self.items.size() + 1, self.embedding_size)(input)
+        for i in range(self.num_main_layers - 1):
+            x = layers.GRU(self.embedding_size, activation='relu', return_sequences=True)(x)
+        for i in range(self.num_dense_layers):
+            x = layers.Dense(self.embedding_size, activation='relu')(x)
+        output = layers.Dense(self.items, name="output", activation=self.output_layer_activation)(x)
+        model = Model(inputs=[input], outputs=[output], name='GRU')
+        return model
+
+
+
+    def get_model(self):
+        if self.model_arch == "mlp_embedding":
+            model = self.get_mlp_model()
+        elif self.model_arch == "gru":
+            model = self.get_gru_model()
+        else:
+            raise Exception(f"unknown model arch {self.model_arch}")
+
+        ndcg_metric = KerasNDCG(self.eval_ndcg_at)
+
+        loss = get_loss(self.loss, self.items.size(),
+                        self.batch_size, self.ndcg_at,
+                        self.loss_internal_dtype, self.loss_lambda_normalization,
+                        lambdarank_pred_truncate=self.loss_pred_truncate,
+                        lambdarank_bce_weight=self.loss_bce_weight)
+        metrics = [ndcg_metric]
+        if self.log_lambdas_len:
+            metrics.append(LambdarankLambdasSum(loss))
+            metrics.append(BCELambdasSum(loss))
+
+
 
         model.compile(optimizer=self.optimizer, loss=loss, metrics=[ndcg_metric])
         return model
-
-    def get_lambdarank_loss(self):
-        return LambdaRankLoss(self.items.size(), self.batch_size, self.sigma, ndcg_at=self.ndcg_at)
-
-    def get_xendcg_loss(self):
-        return XENDCGLoss(self.items.size(), self.batch_size)
-
-    def get_bpr_loss(self):
-        return BPRLoss(max_positives=10)
 
     def get_next_items(self, user_id, limit, features=None):
         actions = self.user_actions[self.users.get_id(user_id)]
