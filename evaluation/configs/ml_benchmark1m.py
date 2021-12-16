b8397b7d83c5c247a030b486487ee304bb039832
Aleksandr Petrov <firexel@gmail.com>
2021-12-16 22:51:14 +0000

    caser config form ml-benchmark1m

--- evaluation/configs/ml_benchmark1m.py
+++ evaluation/configs/ml_benchmark1m.py
@@ -157,10 +157,10 @@ def mlp_historical_embedding(loss, activation_override=None):
                                                 output_layer_activation=activation, target_decay=0.8))
 
 recommenders_raw = {
-    "GRU4rec-BCE": lambda: dnn("gru", "binary_crossentropy"),
-    "GRU4rec-Lambdarank-Vanilla": lambda: dnn("gru", "lambdarank"),
-    "GRU4rec-Lambdarank-Truncated:2500": lambda: dnn("gru", "lambdarank", loss_pred_truncate=2500),
-    "GRU4rec-Lambdarank-Truncated:2500-bce-weight:0.975": lambda: dnn("gru", "lambdarank", loss_pred_truncate=2500, loss_bce_weight=0.975),
+    "CASER-BCE": lambda: dnn("caser", "binary_crossentropy"),
+    "CASER-Lambdarank-Vanilla": lambda: dnn("caser", "lambdarank"),
+    "CASER-Lambdarank-Truncated:2500": lambda: dnn("caser", "lambdarank", loss_pred_truncate=2500),
+    "CASER-Lambdarank-Truncated:2500-bce-weight:0.975": lambda: dnn("caser", "lambdarank", loss_pred_truncate=2500, loss_bce_weight=0.975),
 }
 
 all_recommenders = list(recommenders_raw.keys())
