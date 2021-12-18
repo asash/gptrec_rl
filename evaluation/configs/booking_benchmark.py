f7b641646ae1ba742eb9cfc667fca44cc3962698
Aleksandr Petrov <firexel@gmail.com>
2021-12-18 16:48:55 +0000

    booking benchmark with caser_nouid and last item only training

--- evaluation/configs/booking_benchmark.py
+++ evaluation/configs/booking_benchmark.py
@@ -3,11 +3,7 @@ import random
 from aprec.datasets.booking import get_booking_dataset
 from aprec.recommenders.top_recommender import TopRecommender
 from aprec.recommenders.svd import SvdRecommender
-from aprec.recommenders.salrec.salrec_recommender import SalrecRecommender
-from aprec.recommenders.lightfm import LightFMRecommender
 from aprec.recommenders.dnn_sequential_recommender.dnn_sequential_recommender import DNNSequentialRecommender
-from aprec.recommenders.filter_seen_recommender import FilterSeenRecommender
-from aprec.recommenders.vanilla_bert4rec import VanillaBERT4Rec
 from aprec.evaluation.metrics.precision import Precision
 from aprec.evaluation.metrics.recall import Recall
 from aprec.evaluation.metrics.ndcg import NDCG
@@ -15,101 +11,18 @@ from aprec.evaluation.metrics.mrr import MRR
 from aprec.evaluation.metrics.map import MAP
 from tensorflow.keras.optimizers import Adam
 from aprec.evaluation.metrics.sps import SPS
-from aprec.losses.lambdarank import LambdaRankLoss
-from aprec.recommenders.deep_mf import DeepMFRecommender
 
 DATASET = get_booking_dataset(unix_timestamps=True)[0] 
-
-def deepmf(users_per_sample, items_per_sample, loss, truncation_level=None, bce_weight=0.0):
-    if loss == 'lambdarank':
-        loss = LambdaRankLoss(items_per_sample, users_per_sample,
-                              ndcg_at=50, pred_truncate_at=truncation_level,
-                              bce_grad_weight=bce_weight, remove_batch_dim=True)
-    return FilterSeenRecommender(DeepMFRecommender(users_per_sample, items_per_sample, loss, steps=1500))
 
 USERS_FRACTIONS = [1.]
 
 def top_recommender():
-    return FilterSeenRecommender(TopRecommender())
+    return TopRecommender()
 
 def svd_recommender(k):
-    return FilterSeenRecommender(SvdRecommender(k))
+    return SvdRecommender(k)
 
-def lightfm_recommender(k, loss):
-    return FilterSeenRecommender(LightFMRecommender(k, loss))
-
-def vanilla_bert4rec(num_steps):
-    max_seq_length = 50
-    masked_lm_prob = 0.2
-    max_predictions_per_seq = 20
-    batch_size = 256
-    num_train_steps = num_steps
-
-    prop_sliding_window = 0.5
-    mask_prob = 1.0
-    dupe_factor = 10
-    pool_size = 10
-
-    num_warmup_steps = 100
-    learning_rate = 1e-4
-    random_seed = 31337
-
-    bert_config = {
-        "attention_probs_dropout_prob": 0.2,
-        "hidden_act": "gelu",
-        "hidden_dropout_prob": 0.2,
-        "hidden_size": 64,
-        "initializer_range": 0.02,
-        "intermediate_size": 256,
-        "max_position_embeddings": 200,
-        "num_attention_heads": 2,
-        "num_hidden_layers": 2,
-        "type_vocab_size": 2,
-    }
-    recommender = VanillaBERT4Rec(max_seq_length=max_seq_length,
-                                  masked_lm_prob=masked_lm_prob,
-                                  max_predictions_per_seq=max_predictions_per_seq,
-                                  mask_prob=mask_prob,
-                                  dupe_factor=dupe_factor,
-                                  pool_size=pool_size,
-                                  prop_sliding_window=prop_sliding_window,
-                                  random_seed=random_seed,
-                                  bert_config=bert_config,
-                                  num_warmup_steps=num_warmup_steps,
-                                  num_train_steps=num_train_steps,
-                                  batch_size=batch_size,
-                                  learning_rate=learning_rate)
-    return FilterSeenRecommender(recommender)
-
-def salrec(loss, num_blocks, learning_rate, ndcg_at,
-                session_len,  lambdas_normalization, activation_override=None,
-                loss_pred_truncate=None,
-                loss_bce_weight=0.0,
-                log_lambdas=False
-           ):
-    activation = 'linear' if loss == 'lambdarank' else 'sigmoid'
-    if activation_override is not None:
-        activation = activation_override
-    return FilterSeenRecommender(SalrecRecommender(train_epochs=10000, loss=loss,
-                                                   optimizer=Adam(learning_rate), 
-                                                   early_stop_epochs=10000,
-                                                   batch_size=128, sigma=1.0, ndcg_at=ndcg_at,
-                                                   max_history_len=session_len,
-                                                   output_layer_activation=activation,
-                                                   training_time_limit = 3600,
-                                                   num_blocks=num_blocks,
-                                                   num_target_predictions=5,
-                                                   eval_ndcg_at=40,
-                                                   target_decay=0.8, 
-                                                   loss_lambda_normalization=lambdas_normalization,
-                                                   loss_pred_truncate=loss_pred_truncate,
-                                                   loss_bce_weight=loss_bce_weight, 
-                                                   log_lambdas_len=log_lambdas
-                                                   ))
-
-
-
-def dnn(model_arch, loss,
+def dnn_sequential(model_arch, loss,
            session_len=50,
            ndcg_at = 50,
            activation_override=None,
@@ -119,45 +32,86 @@ def dnn(model_arch, loss,
            num_dense_layers=1,
            loss_pred_truncate=None,
            loss_bce_weight=0.0,
-           log_lambdas=False
+           log_lambdas=False,
+           train_on_last=False,
+           caser_uid = True,
            ):
     activation = 'linear' if loss == 'lambdarank' else 'sigmoid'
     if activation_override is not None:
         activation = activation_override
-    return FilterSeenRecommender(DNNSequentialRecommender(train_epochs=10000, loss=loss,
-                                                          model_arch=model_arch,
-                                                          optimizer=Adam(learning_rate),
-                                                          early_stop_epochs=10000,
-                                                          batch_size=128, sigma=1.0, ndcg_at=ndcg_at,
-                                                          max_history_len=session_len,
-                                                          output_layer_activation=activation,
-                                                          training_time_limit = 3600,
-                                                          eval_ndcg_at=40,
-                                                          target_decay=1.0,
-                                                          loss_lambda_normalization=lambdas_normalization,
-                                                          loss_pred_truncate=loss_pred_truncate,
-                                                          loss_bce_weight=loss_bce_weight,
-                                                          log_lambdas_len=log_lambdas,
-                                                          num_main_layers=num_main_layers,
-                                                          num_dense_layers=num_dense_layers
-                                                          ))
-
-def mlp_historical_embedding(loss, activation_override=None):
-    activation = 'linear' if loss == 'lambdarank' else 'sigmoid'
-    if activation_override is not None:
-        activation = activation_override
-    return FilterSeenRecommender(DNNSequentialRecommender(train_epochs=10000, loss=loss,
-                                                          optimizer=Adam(), early_stop_epochs=100,
-                                                          batch_size=64, sigma=1.0, ndcg_at=40,
-                                                          bottleneck_size=64,
-                                                          max_history_len=150,
-                                                          output_layer_activation=activation, target_decay=0.8))
+    return DNNSequentialRecommender(train_epochs=10000, loss=loss,
+                                    model_arch=model_arch,
+                                    optimizer=Adam(learning_rate),
+                                    early_stop_epochs=10000,
+                                    batch_size=128, sigma=1.0, ndcg_at=ndcg_at,
+                                    max_history_len=session_len,
+                                    output_layer_activation=activation,
+                                    training_time_limit = 3600,
+                                    eval_ndcg_at=40,
+                                    target_decay=1.0,
+                                    loss_lambda_normalization=lambdas_normalization,
+                                    loss_pred_truncate=loss_pred_truncate,
+                                    loss_bce_weight=loss_bce_weight,
+                                    log_lambdas_len=log_lambdas,
+                                    num_main_layers=num_main_layers,
+                                    num_dense_layers=num_dense_layers,
+                                    train_on_last_item_only=train_on_last,
+                                    caser_use_user_id=caser_uid
+                                    )
 
 recommenders_raw = {
-    "GRU4rec-BCE": lambda: dnn("gru", "binary_crossentropy"),
-    "GRU4rec-Lambdarank-Vanilla": lambda: dnn("gru", "lambdarank"),
-    "GRU4rec-Lambdarank-Truncated:2500": lambda: dnn("gru", "lambdarank", loss_pred_truncate=2500),
-    "GRU4rec-Lambdarank-Truncated:2500-bce-weight:0.975": lambda: dnn("gru", "lambdarank", loss_pred_truncate=2500, loss_bce_weight=0.975),
+    #GRU-default
+    "GRU4rec-BCE": lambda: dnn_sequential("gru", "binary_crossentropy"),
+    "GRU4rec-Lambdarank-Vanilla": lambda: dnn_sequential("gru", "lambdarank"),
+    "GRU4rec-Lambdarank-Truncated:2500": lambda: dnn_sequential("gru", "lambdarank", loss_pred_truncate=2500),
+    "GRU4rec-Lambdarank-Truncated:2500-bce-weight:0.975": lambda: dnn_sequential("gru", "lambdarank", loss_pred_truncate=2500, loss_bce_weight=0.975),
+
+    #GRU-train_on_last
+    "GRU4rec-lastOnly-BCE": lambda: dnn_sequential("gru", "binary_crossentropy", train_on_last=True),
+    "GRU4rec-lastOnly-Lambdarank-Vanilla": lambda: dnn_sequential("gru", "lambdarank", train_on_last=True),
+    "GRU4rec-lastOnly-Lambdarank-Truncated:2500-lastOnly": lambda: dnn_sequential("gru", "lambdarank", loss_pred_truncate=2500, train_on_last=True),
+    "GRU4rec-lastOnly-Lambdarank-Truncated:2500-bce-weight:0.975": lambda: dnn_sequential("gru", "lambdarank",
+                                                                                 loss_pred_truncate=2500,
+                                                                                 loss_bce_weight=0.975, train_on_last=True),
+
+    # CASER-default
+    "CASER-BCE": lambda: dnn_sequential("caser", "binary_crossentropy"),
+    "CASER-Lambdarank-Vanilla": lambda: dnn_sequential("caser", "lambdarank"),
+    "CASER-Lambdarank-Truncated:2500": lambda: dnn_sequential("caser", "lambdarank", loss_pred_truncate=2500),
+    "CASER-Lambdarank-Truncated:2500-bce-weight:0.975": lambda: dnn_sequential("caser", "lambdarank",
+                                                                                 loss_pred_truncate=2500,
+                                                                                 loss_bce_weight=0.975),
+
+    # CASER-train_on_last
+    "CASER-lastOnly-BCE": lambda: dnn_sequential("caser", "binary_crossentropy", train_on_last=True),
+    "CASER-lastOnly-Lambdarank-Vanilla": lambda: dnn_sequential("caser", "lambdarank", train_on_last=True),
+    "CASER-lastOnly-Lambdarank-Truncated:2500-lastOnly": lambda: dnn_sequential("caser", "lambdarank",
+                                                                                  loss_pred_truncate=2500,
+                                                                                  train_on_last=True),
+    "CASER-lastOnly-Lambdarank-Truncated:2500-bce-weight:0.975": lambda: dnn_sequential("caser", "lambdarank",
+                                                                                          loss_pred_truncate=2500,
+                                                                                          loss_bce_weight=0.975,
+                                                                                          train_on_last=True),
+
+    # CASER-nouid-nouid-default
+    "CASER-nouid-BCE": lambda: dnn_sequential("caser", "binary_crossentropy", caser_uid=False),
+    "CASER-nouid-Lambdarank-Vanilla": lambda: dnn_sequential("caser", "lambdarank", caser_uid=False),
+    "CASER-nouid-Lambdarank-Truncated:2500": lambda: dnn_sequential("caser", "lambdarank", loss_pred_truncate=2500, caser_uid=False),
+    "CASER-nouid-Lambdarank-Truncated:2500-bce-weight:0.975": lambda: dnn_sequential("caser", "lambdarank",
+                                                                               loss_pred_truncate=2500,
+                                                                               loss_bce_weight=0.975, caser_uid=False),
+
+    # CASER-nouid-nouid-train_on_last
+    "CASER-nouid-lastOnly-BCE": lambda: dnn_sequential("caser", "binary_crossentropy", train_on_last=True, caser_uid=False),
+    "CASER-nouid-lastOnly-Lambdarank-Vanilla": lambda: dnn_sequential("caser", "lambdarank", train_on_last=True, caser_uid=False),
+    "CASER-nouid-lastOnly-Lambdarank-Truncated:2500-lastOnly": lambda: dnn_sequential("caser", "lambdarank",
+                                                                                loss_pred_truncate=2500,
+                                                                                train_on_last=True, caser_uid=False),
+    "CASER-nouid-lastOnly-Lambdarank-Truncated:2500-bce-weight:0.975": lambda: dnn_sequential("caser", "lambdarank",
+                                                                                        loss_pred_truncate=2500,
+                                                                                        loss_bce_weight=0.975,
+                                                                                        train_on_last=True, caser_uid=False),
+
 }
 
 all_recommenders = list(recommenders_raw.keys())
