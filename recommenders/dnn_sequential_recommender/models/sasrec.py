57f88265166d41e454b73d79326fcae845a00dce
Aleksandr Petrov <firexel@gmail.com>
2021-12-23 14:14:02 +0000

    replaced multihead attention in sasrec with original implementation

--- recommenders/dnn_sequential_recommender/models/sasrec.py
+++ recommenders/dnn_sequential_recommender/models/sasrec.py
@@ -11,13 +11,12 @@ import tensorflow as tf
 #https://github.com/kang205/SASRec
 class SASRec(SequentialRecsysModel):
     def __init__(self, output_layer_activation='linear', embedding_size=64,
-                 max_history_len=64, l2_emb=0.0, dropout_rate=0.5, num_blocks=2, num_heads=1, vanilla_sasrec=True):
+                 max_history_len=64, l2_emb=0.0, dropout_rate=0.5, num_blocks=2, num_heads=1):
         super().__init__(output_layer_activation, embedding_size, max_history_len)
         self.l2_emb = l2_emb
         self.dropout_rate = dropout_rate
         self.num_blocks = num_blocks
         self.num_heads = num_heads
-        self.vanilla_sasrec = vanilla_sasrec
 
 
     def get_model(self):
@@ -54,8 +53,12 @@ class OwnSasrecModel(tensorflow.keras.Model):
         for i in range(self.num_blocks):
             block_layers = {
                 "first_norm": layers.LayerNormalization(),
-                "attention": layers.MultiHeadAttention(num_heads=self.num_heads,
-                                                       dropout=self.dropout_rate, key_dim=self.embedding_size),
+                "attention_layers": {
+                    "query_proj": layers.Dense(self.embedding_size, activation='linear'),
+                    "key_proj": layers.Dense(self.embedding_size, activation='linear'),
+                    "val_proj": layers.Dense(self.embedding_size, activation='linear'),
+                    "dropout": layers.Dropout(self.dropout_rate),
+                },
                 "second_norm": layers.LayerNormalization(),
                 "dense": layers.Dense(self.embedding_size, activation='relu'),
                 "dropout": layers.Dropout(self.dropout_rate)
@@ -71,8 +74,8 @@ class OwnSasrecModel(tensorflow.keras.Model):
         x = self.attention_blocks[i]["first_norm"](seq)
         queries = x
         keys = seq
-        values = seq
-        x = self.attention_blocks[i]["attention"](queries, values, keys)
+        x = self.multihead_attention(queries, keys, self.attention_blocks[i]["attention_layers"],
+                                     causality=True) + queries
         residual = x
         x = self.attention_blocks[i]["second_norm"](x)
         x = self.attention_blocks[i]["dense"](x)
@@ -97,5 +100,63 @@ class OwnSasrecModel(tensorflow.keras.Model):
         output = self.output_activation(output)
         return output
 
+    #this version of multihead attention was ported from the original SASRec implementation,
+    # as it does some non-standard transformations, including 'casuality' one
+    def multihead_attention(self,
+                            queries,
+                            keys,
+                            attention_layers,
+                            causality=False,
+                            ):
 
+        Q = attention_layers["query_proj"](queries) # (N, T_q, C)
+        K = attention_layers["key_proj"](keys)  # (N, T_k, C)
+        V = attention_layers["val_proj"](keys) # (N, T_k, C)
+
+        # Split and concat
+        Q_ = tf.concat(tf.split(Q, self.num_heads, axis=2), axis=0)  # (h*N, T_q, C/h)
+        K_ = tf.concat(tf.split(K, self.num_heads, axis=2), axis=0)  # (h*N, T_k, C/h)
+        V_ = tf.concat(tf.split(V, self.num_heads, axis=2), axis=0)  # (h*N, T_k, C/h)
+
+        # Multiplication
+        outputs = tf.matmul(Q_, tf.transpose(K_, [0, 2, 1]))  # (h*N, T_q, T_k)
+
+        # Scale
+        outputs = outputs / (K_.get_shape().as_list()[-1] ** 0.5)
+
+        # Key Masking
+        key_masks = tf.sign(tf.reduce_sum(tf.abs(keys), axis=-1))  # (N, T_k)
+        key_masks = tf.tile(key_masks, [self.num_heads, 1])  # (h*N, T_k)
+        key_masks = tf.tile(tf.expand_dims(key_masks, 1), [1, tf.shape(queries)[1], 1])  # (h*N, T_q, T_k)
+
+        paddings = tf.ones_like(outputs) * (-2 ** 32 + 1)
+        outputs = tf.where(tf.equal(key_masks, 0), paddings, outputs)  # (h*N, T_q, T_k)
+
+        # Causality = Future blinding
+        if causality:
+            diag_vals = tf.ones_like(outputs[0, :, :])  # (T_q, T_k)
+            tril = tf.linalg.LinearOperatorLowerTriangular(diag_vals).to_dense()  # (T_q, T_k)
+            masks = tf.tile(tf.expand_dims(tril, 0), [tf.shape(outputs)[0], 1, 1])  # (h*N, T_q, T_k)
+
+            paddings = tf.ones_like(masks) * (-2 ** 32 + 1)
+            outputs = tf.where(tf.equal(masks, 0), paddings, outputs)  # (h*N, T_q, T_k)
+
+        # Activation
+        outputs = tf.nn.softmax(outputs)  # (h*N, T_q, T_k)
+
+        # Query Masking
+        query_masks = tf.sign(tf.reduce_sum(tf.abs(queries), axis=-1))  # (N, T_q)
+        query_masks = tf.tile(query_masks, [self.num_heads, 1])  # (h*N, T_q)
+        query_masks = tf.tile(tf.expand_dims(query_masks, -1), [1, 1, tf.shape(keys)[1]])  # (h*N, T_q, T_k)
+        outputs *= query_masks  # broadcasting. (N, T_q, C)
+
+        # Dropouts
+        outputs = attention_layers["dropout"](outputs)
+
+        # Weighted sum
+        outputs = tf.matmul(outputs, V_)  # ( h*N, T_q, C/h)
+
+        # Restore shape
+        outputs = tf.concat(tf.split(outputs, self.num_heads, axis=0), axis=2)  # (N, T_q, C)
+        return outputs
 
