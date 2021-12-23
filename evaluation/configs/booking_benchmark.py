a042b2619b3bda925f3032068405b6c17ee67967
Aleksandr Petrov <firexel@gmail.com>
2021-12-23 17:27:57 +0000

    fixed unix timestamps in booking config

--- evaluation/configs/booking_benchmark.py
+++ evaluation/configs/booking_benchmark.py
@@ -2,7 +2,7 @@ from aprec.datasets.booking import get_booking_dataset
 from aprec.evaluation.split_actions import LeaveOneOut
 from aprec.evaluation.configs.common_benhmark_config import *
 
-DATASET = get_booking_dataset()[0]
+DATASET = get_booking_dataset(unix_timestamps=True)[0]
 N_VAL_USERS=1024
 MAX_TEST_USERS=8196
 SPLIT_STRATEGY = LeaveOneOut(MAX_TEST_USERS)
