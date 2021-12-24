14bbbe2991dcf9c87871a108f41642335558e7a5
Aleksandr Petrov <firexel@gmail.com>
2021-12-24 09:57:50 +0000

    hyperparameters search for sasrec

--- evaluation/configs/common_benhmark_config.py
+++ evaluation/configs/common_benhmark_config.py
@@ -1,3 +1,7 @@
+import random
+
+import numpy as np
+
 from aprec.recommenders.top_recommender import TopRecommender
 from aprec.recommenders.svd import SvdRecommender
 from aprec.recommenders.dnn_sequential_recommender.dnn_sequential_recommender import DNNSequentialRecommender
@@ -7,8 +11,6 @@ from aprec.losses.bce import BCELoss
 from aprec.recommenders.dnn_sequential_recommender.models.sasrec import SASRec
 
 from aprec.losses.lambda_gamma_rank import LambdaGammaRankLoss
-from aprec.recommenders.dnn_sequential_recommender.models.caser import Caser
-from aprec.recommenders.dnn_sequential_recommender.models.gru4rec import GRU4Rec
 
 
 from aprec.evaluation.metrics.ndcg import NDCG
@@ -33,15 +35,16 @@ def lightfm_recommender(k, loss):
     return LightFMRecommender(k, loss, num_threads=32)
 
 
-def dnn(model_arch, loss, learning_rate=0.001, last_only=False):
+def dnn(model_arch, loss, learning_rate=0.001, last_only=False, training_time_limit=600):
     return DNNSequentialRecommender(train_epochs=10000, loss=loss,
                                                           model_arch=model_arch,
                                                           optimizer=Adam(learning_rate),
                                                           early_stop_epochs=100,
                                                           batch_size=128,
-                                                          training_time_limit=600,
+                                                          training_time_limit=training_time_limit,
                                                           eval_ndcg_at=40,
                                                           target_decay=1.0,
+
                                                           train_on_last_item_only=last_only
                                                           )
 
@@ -52,30 +55,44 @@ def vanilla_bert4rec(time_limit):
 
 
 recommenders = {
-    "SASRec-Lambdarank-Truncated:2500-bce-weight:0.975": lambda: dnn(
-        SASRec(), LambdaGammaRankLoss(pred_truncate_at=2500, bce_grad_weight=0.975)),
-    "SASRec-BCE": lambda: dnn(
-        SASRec(), BCELoss()),
-    "SASRec-lastonly-BCE": lambda: dnn(
+   "SASRec-lastonly:True-BCE": lambda: dnn(
         SASRec(), BCELoss(), last_only=True),
-    "SASRec-Lambdarank-Truncated:2500": lambda: dnn(SASRec(),
-                                                    LambdaGammaRankLoss(pred_truncate_at=2500)),
 
-    "top_recommender": top_recommender,
-    "svd_recommender": lambda: svd_recommender(128),
-    "lightfm_recommender": lambda: lightfm_recommender(128, 'bpr'),
-
-    "CASER-nouid-Lambdarank-Truncated:2500": lambda: dnn(Caser(requires_user_id=False),
-                                                         LambdaGammaRankLoss(pred_truncate_at=2500)),
-    "CASER-nouid-Lambdarank-Truncated:2500-bce-weight:0.975": lambda: dnn(
-        Caser(requires_user_id=False), LambdaGammaRankLoss(pred_truncate_at=2500, bce_grad_weight=0.975)),
-
-    "GRU4Rec-Lambdarank-Truncated:2500": lambda: dnn(GRU4Rec(), LambdaGammaRankLoss(pred_truncate_at=2500)),
-    "GRU4Rec-Lambdarank-Truncated:2500-bce-weight:0.975": lambda: dnn(GRU4Rec(), LambdaGammaRankLoss(
-        pred_truncate_at=2500,
-        bce_grad_weight=0.975)),
-
+    "SASRec-lastonly:False-BCE": lambda: dnn(
+        SASRec(), BCELoss(), last_only=True)
 }
+for i in range(1000):
+    dropout_rate = bool([0.0, 0.1, 0.2, 0.3, 0.4, 0.5, 0.6])
+    num_blocks = int(np.random.choice([1, 2, 3, 4, 5]))
+    num_heads = int(np.random.choice([1, 2, 4, 8, 16, 32]))
+    emb_size = int(np.random.choice([32, 64, 128, 256, 512]))
+    seq_len = int(np.random.choice([4, 6, 8, 16, 32, 64]))
+
+
+    model = SASRec(num_blocks=num_blocks,
+                   num_heads=num_heads,
+                   embedding_size=emb_size,
+                   dropout_rate=dropout_rate,
+                   max_history_len=seq_len)
+
+    model_name = f"SasRec-blocks:{num_blocks}" \
+                 f"-heads:{num_heads}-seq_len:{seq_len}-dropout:{dropout_rate}" \
+                 f"-dim:{emb_size}"
+
+    bce_weight = np.random.choice([0.0, 0.9, 0.95, 0.975, 0.99, 0.995, 0.999, 0.9995, 0.9999])
+    truncation = int(np.random.choice([100, 200, 400, 800, 1500, 2500, 5000, 10000]))
+    loss = LambdaGammaRankLoss(pred_truncate_at=truncation, bce_grad_weight=bce_weight)
+    loss_name=f"Lambdarank-trunc:{truncation}-bce_weight:{bce_weight}"
+
+    trainig_time_minutes = int(np.random.choice([1, 2, 5, 10, 15]))
+    last_only = bool(np.random.choice([True, False]))
+    training_properties=f"TimeLimit:{trainig_time_minutes}-lastonly:{last_only}`"
+    recommender_name = "-".join([model_name, loss_name, training_properties])
+
+    recommenders[recommender_name] = lambda: dnn(model_arch=model,
+                                                 loss=loss, last_only=last_only,
+                                                 training_time_limit=trainig_time_minutes*60)
+
 
 METRICS = [HIT(1), HIT(5), HIT(10), NDCG(5), NDCG(10), MRR()]
 SAMPLED_METRICS_ON=101
