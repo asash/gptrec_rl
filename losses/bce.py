9bc904dcf554b011cfad393a71fae40c98a7d044
Aleksandr Petrov <firexel@gmail.com>
2022-01-13 17:57:06 +0000

    fixed nan loss value by increasing the epsilion in BCE LOSS

--- losses/bce.py
+++ losses/bce.py
@@ -1,6 +1,7 @@
 from aprec.losses.loss import Loss
 import tensorflow as tf
 from tensorflow.keras.losses import BinaryCrossentropy
+import tensorflow.keras.backend as K
 
 
 class BCELoss(Loss):
@@ -8,14 +9,19 @@ class BCELoss(Loss):
         super().__init__()
         self.__name__ = "BCE"
         self.less_is_better = True
+        self.eps = tf.constant(1e-16, 'float32')
 
-    def __call__(self, y_true, y_pred):
-        EPS = 1e-24
-        y_true_float = tf.cast(y_true, 'float32')
-        is_target = tf.cast((y_true_float >= -EPS), 'float32')
-        trues = y_true_float*is_target
-        pos = -trues*tf.math.log((tf.sigmoid(y_pred) + EPS)) * is_target
-        neg = -(1.0 - trues)*tf.math.log((1.0 - tf.sigmoid(y_pred)) + EPS) * is_target
+    def __call__(self, y_true_raw, y_pred):
+        y_true = tf.cast(y_true_raw, 'float32')
+        is_target = tf.cast((y_true >= -self.eps), 'float32')
+        trues = y_true*is_target
+        pos = -trues*tf.math.log((tf.sigmoid(y_pred) + self.eps)) * is_target
+        neg = -(1.0 - trues)*tf.math.log((1.0 - tf.sigmoid(y_pred)) + self.eps) * is_target
         num_targets = tf.reduce_sum(is_target)
-        res_sum = tf.math.divide_no_nan(tf.reduce_sum(pos + neg), num_targets)
+        ce_sum = tf.reduce_sum(pos + neg)
+        res_sum = tf.math.divide_no_nan(ce_sum, num_targets)
+        # K.print_tensor(y_true, "y_true", summarize=100000000)
+        # K.print_tensor(y_pred, "y_pred", summarize=10000000)
+        # K.print_tensor(pos, "pos", summarize=100000000)
+        # K.print_tensor(pos, "neg", summarize=1000000000)
         return res_sum
--- tests/lossess/test_bce_loss.py
+++ tests/lossess/test_bce_loss.py
@@ -1,11 +1,15 @@
 from tensorflow.keras.losses import BinaryCrossentropy
-import tensorflow.keras.backend as K
 import tensorflow as tf
+from aprec.tests.bce_bad_sample import y_true as bad_y_true
+from aprec.tests.bce_bad_sample import y_pred as bad_y_pred
 from aprec.losses.bce import BCELoss
 import unittest
 
 class TestBCELoss(unittest.TestCase):
     def test_bce_loss(self):
+        loss = float(BCELoss()(bad_y_true, bad_y_pred))
+        print(loss)
+
         y_true = tf.constant([-1., -1, -1, -1])
         y_pred = [-50.0, -50, -50, -50]
         loss = float(BCELoss()(y_true, y_pred))
@@ -15,16 +19,15 @@ class TestBCELoss(unittest.TestCase):
         y_pred = [0.1, 0.2, 0.3, 0.4]
         loss = float(BCELoss()(y_true, y_pred))
         keras_loss = float(BinaryCrossentropy(from_logits=True)(y_true, y_pred))
-        self.assertAlmostEqual(loss, keras_loss)
+        self.assertAlmostEqual(loss, keras_loss, 5)
 
         y_true = tf.constant([1., 0, 1, 0])
         y_pred = [-50.0, -50, -50, -50]
         loss = float(BCELoss()(y_true, y_pred))
         keras_loss = float(BinaryCrossentropy(from_logits=True)(y_true, y_pred))
-        self.assertAlmostEqual(loss, 24.997413635253906)
+        self.assertAlmostEqual(loss, 18.420679092407227)
 
 
-
-
-
+if __name__ == "__main__":
+    unittest.main()
 
