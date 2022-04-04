ad5d101870d2eb922286ca8238cb5a5506d63778
Aleksandr Petrov <firexel@gmail.com>
2022-04-04 19:03:19 +0100

    added yelp dataset

--- datasets/datasets_register.py
+++ datasets/datasets_register.py
@@ -5,6 +5,7 @@ from aprec.datasets.booking import get_booking_dataset
 from aprec.datasets.movielens100k import get_movielens100k_actions
 from aprec.datasets.movielens20m import get_movielens20m_actions
 from aprec.datasets.gowalla import get_gowalla_dataset
+from aprec.datasets.yelp import get_yelp_dataset
 from aprec.datasets.mts_kion import get_mts_kion_dataset
 
 class DatasetsRegister(object):
@@ -16,7 +17,8 @@ class DatasetsRegister(object):
         "ml-100k": lambda: get_movielens100k_actions(min_rating=0.0),
         "booking": lambda: get_booking_dataset(unix_timestamps=True)[0],
         "gowalla": get_gowalla_dataset,
-        "mts_kion": lambda: get_mts_kion_dataset()
+        "mts_kion": lambda: get_mts_kion_dataset(),
+        "yelp": get_yelp_dataset,
     }
 
     def __getitem__(self, item):
