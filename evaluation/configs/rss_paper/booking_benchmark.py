96ebaab7d7517516ca0b0d6429f83d100682316a
Aleksandr Petrov <firexel@gmail.com>
2022-04-08 09:35:30 +0100

    Added "warm5" versions of the datasets.

--- evaluation/configs/rss_paper/booking_benchmark.py
+++ evaluation/configs/rss_paper/booking_benchmark.py
@@ -1,7 +1,7 @@
 from aprec.evaluation.split_actions import LeaveOneOut
 from aprec.evaluation.configs.rss_paper.common_benchmark_config import *
 
-DATASET = "booking"
+DATASET = "booking_warm5"
 N_VAL_USERS=1024
 MAX_TEST_USERS=8196
 SPLIT_STRATEGY = LeaveOneOut(MAX_TEST_USERS)
