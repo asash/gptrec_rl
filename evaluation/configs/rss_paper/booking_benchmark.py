9d7d6aee664e4a8b0d82e7e17592df2f779e8beb
Aleksandr Petrov <firexel@gmail.com>
2022-04-14 08:51:37 +0100

    increased number of test users in the booking config

--- evaluation/configs/rss_paper/booking_benchmark.py
+++ evaluation/configs/rss_paper/booking_benchmark.py
@@ -3,6 +3,6 @@ from aprec.evaluation.configs.rss_paper.common_benchmark_config import *
 
 DATASET = "booking_warm5"
 N_VAL_USERS=1024
-MAX_TEST_USERS=8196
+MAX_TEST_USERS=140746
 SPLIT_STRATEGY = LeaveOneOut(MAX_TEST_USERS)
 RECOMMENDERS = get_recommenders(filter_seen=False)
