18eb2c85e4e3d9ea50d2de7b2e58758abc2bfd4f
Aleksandr Petrov <firexel@gmail.com>
2022-04-20 18:40:19 +0100

    added 0.5h and 1h configs for original bert4rec evaluation on ml1m

---
+++ evaluation/configs/bert4rec_repro_paper/ml_1m_original_0.5h.py
@@ -0,0 +1,44 @@
+from aprec.evaluation.samplers.pop_sampler import PopTargetItemsSampler
+from aprec.evaluation.split_actions import LeaveOneOut
+from aprec.recommenders.vanilla_bert4rec import VanillaBERT4Rec
+
+from aprec.evaluation.metrics.ndcg import NDCG
+from aprec.evaluation.metrics.mrr import MRR
+from aprec.evaluation.metrics.map import MAP
+from aprec.evaluation.metrics.hit import HIT
+
+from aprec.recommenders.filter_seen_recommender import FilterSeenRecommender
+
+USERS_FRACTIONS = [1.0]
+
+
+def original_ber4rec(time_limit):
+    recommender = VanillaBERT4Rec(training_time_limit=time_limit)
+    return recommender
+
+HISTORY_LEN=50
+
+recommenders = {
+     "original_bert4rec-0.5h": lambda: original_ber4rec(1800)
+}
+
+TARGET_ITEMS_SAMPLER = PopTargetItemsSampler(101)
+METRICS = [HIT(1), HIT(5), HIT(10), NDCG(5), NDCG(10), MRR(), HIT(4), NDCG(40), MAP(10)]
+
+def get_recommenders(filter_seen: bool, filter_recommenders = set()):
+    result = {}
+    for recommender_name in recommenders:
+        if recommender_name in filter_recommenders:
+                continue
+        if filter_seen:
+            result[recommender_name] =\
+                lambda recommender_name=recommender_name: FilterSeenRecommender(recommenders[recommender_name]())
+        else:
+            result[recommender_name] = recommenders[recommender_name]
+    return result
+
+DATASET = "BERT4rec.ml-1m"
+N_VAL_USERS=2048
+MAX_TEST_USERS=6040
+SPLIT_STRATEGY = LeaveOneOut(MAX_TEST_USERS)
+RECOMMENDERS = get_recommenders(filter_seen=True)
