146e1d3d75cff13ff941ef8ce282a095a891cb11
Aleksandr Petrov <firexel@gmail.com>
2022-05-10 12:00:22 +0100

    Implemented optional negatives sampling in bert4rec samples builder

--- recommenders/dnn_sequential_recommender/target_builders/items_masking_target_builder.py
+++ recommenders/dnn_sequential_recommender/target_builders/items_masking_target_builder.py
@@ -1,4 +1,4 @@
-from random import Random
+from random import Random, sample
 
 import numpy as np
 from aprec.recommenders.dnn_sequential_recommender.target_builders.target_builders import TargetBuilder
@@ -6,19 +6,32 @@ from aprec.recommenders.dnn_sequential_recommender.target_builders.target_builde
 class ItemsMaskingTargetsBuilder(TargetBuilder):
     def __init__(self, random_seed=31337, 
                        relative_positions_encoding = True, 
-                       ignore_value=-100): #-100 is used by default in hugginface's BERT implementation
+                       ignore_value=-100, 
+                       negatives_sampler=None
+                       ): #-100 is used by default in hugginface's BERT implementation
         self.random = Random()
         self.random.seed(random_seed) 
         self.targets = []
         self.ignore_value = ignore_value
         self.relative_positions_encoding = relative_positions_encoding
+        self.negatives_sampler = None
         self.positions = []
+        self.negatives_sampler = negatives_sampler
+
+    def set_n_items(self, n):
+        super().set_n_items(n)
+        if self.negatives_sampler is not None:
+            self.negatives_sampler.set_n_items(n)
 
     def build(self, user_targets):
         targets = []
         positions = []
+        negatives = []
         for seq_len, user in user_targets:
             user_positions = []
+            if self.negatives_sampler is not None:
+                user_negatives = [self.negatives_sampler.default_vector()] * self.sequence_len
+
             user_target = [self.ignore_value] * self.sequence_len
             if self.relative_positions_encoding:
                 split_pos = self.random.randint(self.sequence_len - seq_len, self.sequence_len - 1)
@@ -31,13 +44,41 @@ class ItemsMaskingTargetsBuilder(TargetBuilder):
             positions.append(user_positions)
             for pos in user:
                 user_target[pos[0]] = pos[1][1]
-
+                if self.negatives_sampler is not None:
+                    user_negatives[pos[0]] = self.negatives_sampler.sample_negatives(pos[1][1])
             targets.append(user_target)
-
+            if self.negatives_sampler is not None:
+                negatives.append(user_negatives)
         self.positions = np.array(positions)
         self.targets = np.array(targets)
-
-
+        self.negatives = np.array(negatives)
 
     def get_targets(self, start, end):
-        return [self.targets[start:end], self.positions[start:end]], self.targets[start:end]
+        if self.negatives_sampler is None:
+            return [self.targets[start:end], self.positions[start:end]], self.targets[start:end]
+        else:
+            return [self.targets[start:end], self.positions[start:end], self.negatives[start:end]], self.targets[start:end]
+
+
+class RandomNegativesSampler(object):
+    def __init__(self, sample_size, seed=31337, default_value=-100):
+        self.sample_size = sample_size
+        self.random = Random()
+        self.random.seed(seed) 
+        self.def_vector = [default_value] * sample_size 
+    
+    def default_vector(self):
+        return self.def_vector
+
+    def set_n_items(self, n):
+        self.n_items = n
+
+    def sample_negatives(self, positive):
+        result = []
+        while len(result) < self.sample_size:
+            sample = self.random.randint(0, self.n_items - 1)
+            if sample != positive:
+                result.append(sample)
+        return result
+
+    
--- tests/test_items_masking_target_builder.py
+++ tests/test_items_masking_target_builder.py
@@ -1,10 +1,10 @@
 import unittest
 import numpy as np
 
-from aprec.recommenders.dnn_sequential_recommender.target_builders.items_masking_target_builder import ItemsMaskingTargetsBuilder 
 
 class TestItemsMaskingTargetBuilder(unittest.TestCase):
     def test_target_builder(self):
+        from aprec.recommenders.dnn_sequential_recommender.target_builders.items_masking_target_builder import ItemsMaskingTargetsBuilder
         targets_builder = ItemsMaskingTargetsBuilder(relative_positions_encoding=False)
         targets_builder.set_sequence_len(5)
         targets_builder.set_n_items(10)
@@ -12,9 +12,33 @@ class TestItemsMaskingTargetBuilder(unittest.TestCase):
         expected_targets = np.array([[-100, 3, -100, 5, -100], [-100, 6, -100, -100, -100]])
         expected_positions = np.array([[1, 2, 3, 4, 5], [1, 2, 3, 4, 5]])
         extra_inputs, target = targets_builder.get_targets(0, 2)
+        self.assertEqual(len(extra_inputs), 2)
+        self.assertEquals(len(extra_inputs), 2) # labels, positions
         self.assertTrue(np.all(expected_targets == target))
         self.assertTrue(np.all(extra_inputs[0] == target))
         self.assertTrue(np.all(extra_inputs[1] == expected_positions))
+
+    def test_target_builder_random_negatives(self):
+        from aprec.recommenders.dnn_sequential_recommender.target_builders.items_masking_target_builder import ItemsMaskingTargetsBuilder, RandomNegativesSampler 
+        n_items = 10
+        negatves_sampler = RandomNegativesSampler(3) 
+        targets_builder = ItemsMaskingTargetsBuilder(relative_positions_encoding=False, negatives_sampler=negatves_sampler)
+        targets_builder.set_sequence_len(5)
+        targets_builder.set_n_items(n_items)
+        targets_builder.build([(4, [(1, (1, 3)), (3, (3, 5))]), (3, [(1, (1, 6))])])
+        expected_targets = np.array([[-100, 3, -100, 5, -100], [-100, 6, -100, -100, -100]])
+        expected_positions = np.array([[1, 2, 3, 4, 5], [1, 2, 3, 4, 5]])
+        expected_negatives = np.array([[[-100, -100, -100], [   8,    2,    4], [-100, -100, -100], [   9,    0,   3], [-100, -100, -100]],
+                                       [[-100, -100, -100],  [   2,    5,    1],  [-100, -100, -100], [-100, -100, -100],  [-100, -100, -100]]])
+        extra_inputs, target = targets_builder.get_targets(0, 2)
+        self.assertEquals(len(extra_inputs), 3) # labels, positions, sampled negatives
+        sampled_negatives = extra_inputs[2]
+        self.assertTrue(np.all(expected_negatives == sampled_negatives))
+        self.assertEqual(len(extra_inputs), 3)
+        self.assertTrue(np.all(expected_targets == target))
+        self.assertTrue(np.all(extra_inputs[0] == target))
+        self.assertTrue(np.all(extra_inputs[1] == expected_positions))
+
 
 if __name__ == "__main__":
     unittest.main()
