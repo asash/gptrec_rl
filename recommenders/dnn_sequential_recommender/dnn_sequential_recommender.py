690f3c5c490b4a71deb3a59b6081615056ac8943
Aleksandr Petrov <firexel@gmail.com>
2022-05-22 20:06:34 +0100

    increased the number of data preparation processes to 8

--- recommenders/dnn_sequential_recommender/dnn_sequential_recommender.py
+++ recommenders/dnn_sequential_recommender/dnn_sequential_recommender.py
@@ -41,8 +41,8 @@ class DNNSequentialRecommender(Recommender):
                  metric = KerasNDCG(40), 
                  train_history_vectorizer:HistoryVectorizer = DefaultHistoryVectrizer(), 
                  pred_history_vectorizer:HistoryVectorizer = DefaultHistoryVectrizer(),
-                 data_generator_processes = 4, 
-                 data_generator_queue_size = 8,
+                 data_generator_processes = 8, 
+                 data_generator_queue_size = 16,
                  ):
         super().__init__()
         self.model_arch = model_arch
