529caa6a018c095792c887114591c2706bb10b71
Aleksandr Petrov <firexel@gmail.com>
2022-06-07 18:54:16 +0100

    Reduced the number of test users on all datasets

--- evaluation/configs/bert4rec_ft/ml20m_bert4rec_ftc.py
+++ evaluation/configs/bert4rec_ft/ml20m_bert4rec_ftc.py
@@ -3,6 +3,6 @@ from aprec.evaluation.configs.bert4rec_ft.common_benchmark_config import *
 
 DATASET = "ml-20m"
 N_VAL_USERS=1024
-MAX_TEST_USERS=138493
+MAX_TEST_USERS=8196
 SPLIT_STRATEGY = LeaveOneOut(MAX_TEST_USERS)
 RECOMMENDERS = get_recommenders(filter_seen=True)
--- evaluation/configs/bert4rec_ft/steam.py
+++ evaluation/configs/bert4rec_ft/steam.py
@@ -3,6 +3,6 @@ from aprec.evaluation.configs.bert4rec_ft.common_benchmark_config import *
 
 DATASET = "BERT4rec.steam"
 N_VAL_USERS=2048
-MAX_TEST_USERS=281428
+MAX_TEST_USERS=8196
 SPLIT_STRATEGY = LeaveOneOut(MAX_TEST_USERS)
 RECOMMENDERS = get_recommenders(filter_seen=True)
