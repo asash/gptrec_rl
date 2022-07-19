a732c4c22c2709b8fdff5fd770164fb170134d2a
Aleksandr Petrov <firexel@gmail.com>
2022-07-19 19:31:23 +0100

    added netflix dataset

--- datasets/datasets_register.py
+++ datasets/datasets_register.py
@@ -6,7 +6,9 @@ from aprec.datasets.bert4rec_datasets import get_bert4rec_dataset
 from aprec.datasets.booking import get_booking_dataset
 from aprec.datasets.movielens100k import get_movielens100k_actions
 from aprec.datasets.movielens20m import get_movielens20m_actions
+from aprec.datasets.movielens25m import get_movielens25m_actions
 from aprec.datasets.gowalla import get_gowalla_dataset
+from aprec.datasets.netflix import get_netflix_dataset
 from aprec.datasets.yelp import get_yelp_dataset
 from aprec.datasets.mts_kion import get_mts_kion_dataset
 from aprec.datasets.dataset_utils import filter_cold_users
@@ -20,21 +22,25 @@ class DatasetsRegister(object):
         "BERT4rec.steam": lambda: get_bert4rec_dataset("steam"),
         "BERT4rec.beauty": lambda: get_bert4rec_dataset("beauty"),
         "ml-20m": lambda: get_movielens20m_actions(min_rating=0.0),
+        "ml-25m": lambda: get_movielens25m_actions(min_rating=0.0),
         "ml-100k": lambda: get_movielens100k_actions(min_rating=0.0),
         "booking": lambda: get_booking_dataset(unix_timestamps=True, mark_control=False)[0],
         "gowalla": get_gowalla_dataset,
         "mts_kion": lambda: get_mts_kion_dataset(),
         "yelp": get_yelp_dataset,
+        "netflix": get_netflix_dataset,
 
         "ml-20m_warm5": lambda: filter_cold_users(get_movielens20m_actions(min_rating=0.0), 5), 
         "booking_warm5": lambda: filter_cold_users(get_booking_dataset(unix_timestamps=True, mark_control=False)[0], 5), 
         "gowalla_warm5": lambda: filter_cold_users(get_gowalla_dataset(), 5), 
         "yelp_warm5": lambda: filter_cold_users(get_yelp_dataset(), 5),
+        "netflix_warm5": lambda: filter_cold_users(DatasetsRegister.read_from_cache("netflix"), 5), 
 
         "ml-20m_warm10": lambda: filter_cold_users(get_movielens20m_actions(min_rating=0.0), 10), 
         "booking_warm10": lambda: filter_cold_users(get_booking_dataset(unix_timestamps=True, mark_control=False)[0], 10), 
         "gowalla_warm10": lambda: filter_cold_users(get_gowalla_dataset(), 10), 
-        "yelp_warm10": lambda: filter_cold_users(get_yelp_dataset(), 10)
+        "yelp_warm10": lambda: filter_cold_users(get_yelp_dataset(), 10),
+        "netflix_warm10": lambda: filter_cold_users(DatasetsRegister.read_from_cache("netflix"), 10), 
     }
 
     @staticmethod
