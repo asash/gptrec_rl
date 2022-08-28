48edc3e98e7bc8fa8fce1efa8bee246a544b9eee
Aleksandr Petrov <firexel@gmail.com>
2022-08-28 09:08:17 +0100

    vit4rec - changed loss to BCE

--- tests/test_vit4rec.py
+++ tests/test_vit4rec.py
@@ -1,5 +1,7 @@
 import unittest
 
+
+
 class TestVit4rec(unittest.TestCase):
     def test_vit4rec(self):
 
@@ -8,11 +10,22 @@ class TestVit4rec(unittest.TestCase):
         from aprec.recommenders.filter_seen_recommender import FilterSeenRecommender
         from aprec.datasets.movielens20m import get_movielens20m_actions
         from aprec.utils.generator_limit import generator_limit
-
+        from aprec.losses.mean_ypred_ploss import MeanPredLoss
+        from aprec.recommenders.dnn_sequential_recommender.history_vectorizers.add_mask_history_vectorizer import AddMaskHistoryVectorizer
+        from aprec.recommenders.dnn_sequential_recommender.target_builders.items_masking_target_builder import ItemsMaskingTargetsBuilder
+        from aprec.recommenders.dnn_sequential_recommender.targetsplitters.items_masking import ItemsMasking
         val_users = ['1', '2', '3', '4', '5', '6', '7', '8', '9', '10']
         model = Vit4Rec()
-        recommender = DNNSequentialRecommender(model, train_epochs=10, early_stop_epochs=5,
-                                               batch_size=5, training_time_limit=100)
+        recommender = DNNSequentialRecommender(model, train_epochs=10000, early_stop_epochs=50000,
+                                               batch_size=5,
+                                               training_time_limit=10, 
+                                               loss = MeanPredLoss(),
+                                               debug=True, sequence_splitter=lambda: ItemsMasking(), 
+                                               targets_builder= lambda: ItemsMaskingTargetsBuilder(relative_positions_encoding=True),
+                                               val_sequence_splitter=lambda: ItemsMasking(force_last=True),
+                                               metric=MeanPredLoss(), 
+                                               pred_history_vectorizer=AddMaskHistoryVectorizer(),
+                                               )
         recommender.set_val_users(val_users)
         recommender = FilterSeenRecommender(recommender)
         for action in generator_limit(get_movielens20m_actions(), 10000):
