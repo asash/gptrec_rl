b46c24c9b7c695a3812ea9423c7ec8b9a4ebe0b0
Aleksandr Petrov <firexel@gmail.com>
2022-10-28 16:39:20 +0100

    added dissimilarity sampler

--- recommenders/dnn_sequential_recommender/target_builders/negative_samplers/__init__.py
+++ recommenders/dnn_sequential_recommender/target_builders/negative_samplers/__init__.py
@@ -3,3 +3,4 @@ from .popularity_based_sampler import PopularityBasedSampler
 from .svd_similarity_sampler import SVDSimilaritySampler
 from .random_negatives_sampler import RandomNegativesSampler
 from .mixture_sampler import MixtureSampler
+from .affinity_dissimilarity_sampler import AffinityDissimilaritySampler
---
+++ recommenders/dnn_sequential_recommender/target_builders/negative_samplers/affinity_dissimilarity_sampler.py
@@ -0,0 +1,32 @@
+import numpy as np
+from aprec.recommenders.dnn_sequential_recommender.target_builders.negative_samplers import NegativesSampler 
+class AffinityDissimilaritySampler(NegativesSampler):
+    def __init__(self, sample_size=400, seed=31337):
+        self.sample_size = sample_size
+        self.random = np.random.default_rng(seed)
+
+    def set_train_sequences(self, train_sequences):
+        EPS = 5 
+        items_counter = np.zeros(self.n_items)
+        item_pairs_counter = np.full((self.n_items, self.n_items), EPS) 
+
+        for i in range(len(train_sequences)):
+            items = {item[1] for item in train_sequences[i]}
+            for item1 in items:
+                items_counter[item1] += 1
+                for item2 in items:
+                    item_pairs_counter[item1, item2] += 1 
+                    if item1 != item2:
+                        item_pairs_counter[item2, item1] += 1 
+        
+        dissim = items_counter / item_pairs_counter
+        for i in range(self.n_items):
+            dissim[i][i] = 0
+        norm = np.expand_dims(np.sum(dissim, axis=1), 1)
+        dissim = dissim / norm
+        self.dissim = dissim
+        self.all_items = np.arange(self.n_items)
+
+    def sample_negatives(self, positive):
+        result = np.random.choice(self.all_items, self.sample_size, p=self.dissim[positive], replace=True)
+        return result
