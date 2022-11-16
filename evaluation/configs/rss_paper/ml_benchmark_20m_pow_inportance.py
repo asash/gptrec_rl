a465e4191db4ccf70f6ade61e280ec5f2fb7f290
Aleksandr Petrov <firexel@gmail.com>
2022-11-16 08:11:16 +0000

    some small changes in batch size/num batches balance

--- evaluation/configs/rss_paper/ml_benchmark_20m_pow_inportance.py
+++ evaluation/configs/rss_paper/ml_benchmark_20m_pow_inportance.py
@@ -2,7 +2,7 @@ from aprec.evaluation.split_actions import LeaveOneOut
 from aprec.evaluation.configs.rss_paper.common_benchmark_config_pow_importance import *
 
 DATASET = "ml-20m_warm5"
-N_VAL_USERS=1024
+N_VAL_USERS=2048
 MAX_TEST_USERS=138493
 SPLIT_STRATEGY = LeaveOneOut(MAX_TEST_USERS)
 RECOMMENDERS = get_recommenders(filter_seen=True)
