b43c2cc779553a3270527f8f6af58f287fc14c19
Alexandr Petrov <firexel@gmail.com>
2022-12-20 14:17:06 +0000

    delete some crap

--- install_openssl_ubuntu2204.sh
+++
@@ -1,8 +0,0 @@
-libssl=libssl1.0.0_1.0.2n-1ubuntu5_amd64.deb 
-libssldev=libssl1.0-dev_1.0.2n-1ubuntu5_amd64.deb
-wget http://security.ubuntu.com/ubuntu/pool/main/o/openssl1.0/$libssl
-wget http://security.ubuntu.com/ubuntu/pool/main/o/openssl1.0/$libssldev
-sudo dpkg  -i $libssl $libssldev 
-cd /lib/x86_64-linux-gnu
-sudo ln -s libssl.so.1.0.0 libssl.so.10
-sudo ln -s libcrypto.so.1.0.0 libcrypto.so.10
