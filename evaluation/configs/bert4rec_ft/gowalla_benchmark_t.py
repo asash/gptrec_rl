1a131d8c84ff236b7d91ee42a255275e071b1182
Aleksandr Petrov <firexel@gmail.com>
2022-12-31 17:35:47 +0000

    increased batch batch size

--- evaluation/configs/bert4rec_ft/gowalla_benchmark_t.py
+++ evaluation/configs/bert4rec_ft/gowalla_benchmark_t.py
@@ -3,7 +3,7 @@ from aprec.evaluation.split_actions import LeaveOneOut
 from aprec.evaluation.configs.bert4rec_ft.common_benchmark_config_sasrec_t_large import *
 
 DATASET = "gowalla_warm5"
-N_VAL_USERS=2048
+N_VAL_USERS=1024
 MAX_TEST_USERS=8196
 SPLIT_STRATEGY = LeaveOneOut(MAX_TEST_USERS)
 RECOMMENDERS = get_recommenders(filter_seen=False)
