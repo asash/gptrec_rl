d2b6e9769d6e1158f11abcf3fd688c282115b68b
Aleksandr Petrov <firexel@gmail.com>
2023-02-05 11:11:03 +0000

    saslift config

---
+++ evaluation/configs/bert4rec_ft/common_benchmark_saslift_saslift.py
@@ -0,0 +1,80 @@
+from aprec.evaluation.metrics.entropy import Entropy
+from aprec.evaluation.metrics.highest_score import HighestScore
+from aprec.evaluation.metrics.model_confidence import Confidence
+from aprec.evaluation.metrics.ndcg import NDCG
+from aprec.evaluation.metrics.mrr import MRR
+from aprec.evaluation.metrics.map import MAP
+from aprec.evaluation.metrics.hit import HIT
+from aprec.evaluation.samplers.pop_sampler import PopTargetItemsWithReplacementSampler
+from aprec.recommenders.filter_seen_recommender import FilterSeenRecommender
+from aprec.recommenders.lightfm import LightFMRecommender
+from aprec.recommenders.sequential.target_builders.positives_sequence_target_builder import PositivesSequenceTargetBuilder
+from aprec.recommenders.top_recommender import TopRecommender
+
+USERS_FRACTIONS = [1.0]
+
+EXTRA_VAL_METRICS = [NDCG(10), HighestScore(), NDCG(40), HIT(1), MRR(), 
+                     Confidence('Softmax'),
+                     Confidence('Sigmoid'),
+                     Entropy('Softmax', 10), 
+                     HIT(10)]
+
+METRICS = [HIT(1), HIT(5), HIT(10), NDCG(5), NDCG(10), MRR(), HIT(4), NDCG(40), MAP(10)]
+#TARGET_ITEMS_SAMPLER = PopTargetItemsWithReplacementSampler(101)
+
+SEQUENCE_LENGTH=200
+EMBEDDING_SIZE=128
+ 
+def deb_sasrec(num_samples=32, t=0.5):
+    from aprec.recommenders.sequential.models.sasrec.saslift import SASLiftConfig
+    from aprec.recommenders.sequential.targetsplitters.shifted_sequence_splitter import ShiftedSequenceSplitter
+    model_config = SASLiftConfig(embedding_size=EMBEDDING_SIZE, vanilla_num_negatives=num_samples, 
+                                vanilla_bce_t=t)
+    return sasrec_style_model(model_config, 
+            ShiftedSequenceSplitter,
+            target_builder=lambda: PositivesSequenceTargetBuilder(SEQUENCE_LENGTH),
+            batch_size=128)
+
+
+def sasrec_style_model(model_config, sequence_splitter, 
+                target_builder,
+                max_epochs=10000, 
+                batch_size=128,
+                ):
+    from aprec.recommenders.sequential.sequential_recommender import SequentialRecommender
+    from aprec.recommenders.sequential.sequential_recommender_config import SequentialRecommenderConfig
+
+    config = SequentialRecommenderConfig(model_config,                       
+                                train_epochs=max_epochs,
+                                early_stop_epochs=200,
+                                batch_size=batch_size,
+                                eval_batch_size=256, #no need for gradients, should work ok
+                                validation_batch_size=256,
+                                max_batches_per_epoch=256,
+                                sequence_splitter=sequence_splitter, 
+                                targets_builder=target_builder, 
+                                use_keras_training=True,
+                                extra_val_metrics=EXTRA_VAL_METRICS, 
+                                sequence_length=SEQUENCE_LENGTH
+                                )
+    
+    return SequentialRecommender(config)
+
+
+recommenders = {}
+
+for num_samples in [1, 4, 16, 64, 256]:
+        for t in [0.0, 0.25, 0.5, 0.75, 1.0]:
+                recommenders[f"SASLift-samples:{num_samples}-t:{t}"] = lambda num_samples=num_samples, t=t: deb_sasrec(num_samples=num_samples, t=t)
+
+def get_recommenders(filter_seen: bool):
+    result = {}
+    all_recommenders = list(recommenders.keys())
+    for recommender_name in all_recommenders:
+        if filter_seen:
+            result[recommender_name] =\
+                lambda recommender_name=recommender_name: FilterSeenRecommender(recommenders[recommender_name]())
+        else:
+            result[recommender_name] = recommenders[recommender_name]
+    return result
+
