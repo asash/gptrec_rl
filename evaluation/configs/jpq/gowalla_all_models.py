f2e3e642a8d9901d2acadb48f50d6ff863d5dc3b
Sasha Petrov <firexel@gmail.com>
2023-05-19 15:07:17 +0000

    only sasrec for gowalla

--- evaluation/configs/jpq/gowalla_all_models.py
+++ evaluation/configs/jpq/gowalla_all_models.py
@@ -1,5 +1,5 @@
 from aprec.evaluation.split_actions import LeaveOneOut
-from aprec.evaluation.configs.jpq.common_benchmark_config_all_models import *
+from aprec.evaluation.configs.jpq.common_benchmark_config_all_models_sas import *
 
 DATASET = "gowalla_warm5"
 N_VAL_USERS=1024
