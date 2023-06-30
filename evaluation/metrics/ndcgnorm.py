f234fee7020594d486b0ef0d501177ae96b49ef0
Aleksandr Petrov <firexel@gmail.com>
2023-06-30 17:04:49 +0100

    rescaled ndcg

--- evaluation/metrics/ndcgnorm.py
+++ evaluation/metrics/ndcgnorm.py
@@ -1,32 +1,16 @@
 import math
+
+from aprec.evaluation.metrics.ndcg import NDCG
 from .metric import Metric
 
+# This is a hack to make the NDCG metric return values in the range [-1, 1] instead of [0, 1]
 class NDCGNorm(Metric):
     def __init__(self, k):
         self.name = "ndcg@{}".format(k)
+        self.regular_ndcg = NDCG(k)
         self.k = k
         
     def __call__(self, recommendations, actual_actions):
-        if(len(recommendations) == 0):
-            return 0
-        actual_set = set([action.item_id for action in actual_actions])
-        recommended = [recommendation[0] for recommendation in recommendations[:self.k]]
-        cool = set(recommended).intersection(actual_set)
-        if len(cool) == 0:
-            return 0
-        ideal_rec = sorted(recommended, key = lambda x: not(x in actual_set))
-        return (NDCGNorm.dcg(recommended, actual_set)/NDCGNorm.dcg(ideal_rec, actual_set) - 0.5) * 2
-         
-
-    @staticmethod
-    def dcg(id_list, relevant_id_set):
-        result = 0.0
-        for idx in range(len(id_list)):
-            i = idx + 1
-            if (id_list[idx]) in relevant_id_set:
-                result += 1 / math.log2(i+1)
-        return result
-
-
+        return (self.regular_ndcg(recommendations, actual_actions) - 0.5) * 2  
 
 
