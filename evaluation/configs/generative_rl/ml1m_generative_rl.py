c5a9207b635102076df6be269a3e92ef1ef687ca
Aleksandr Petrov <firexel@gmail.com>
2023-07-08 21:41:30 +0100

    200 steps early stop

--- evaluation/configs/generative_rl/ml1m_generative_rl.py
+++ evaluation/configs/generative_rl/ml1m_generative_rl.py
@@ -39,7 +39,7 @@ def generative_tuning_recommender():
         model_config = RLGPT2RecConfig(transformer_blocks=3, embedding_size=256, tokenizer='id', tokens_per_item=1, values_per_dim=3500, attention_heads=4)
         pre_training_recommender = lambda: FilterSeenRecommender(LightFMRecommender(num_latent_components=256))
 
-        recommender_config = SequentialRecommenderConfig(model_config, train_epochs=200, early_stop_epochs=1,
+        recommender_config = SequentialRecommenderConfig(model_config, train_epochs=200, early_stop_epochs=200,
                                                batch_size=128,
                                                training_time_limit=200000,  
                                                sequence_splitter=IdSplitter, 
