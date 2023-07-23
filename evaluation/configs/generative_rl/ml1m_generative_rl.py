4c312343d97fe0ac86ade2be17ca08fbe379bce6
Aleksandr Petrov <firexel@gmail.com>
2023-07-23 15:12:08 +0100

    this config will run during the holiday

--- evaluation/configs/generative_rl/ml1m_generative_rl.py
+++ evaluation/configs/generative_rl/ml1m_generative_rl.py
@@ -1,32 +1,19 @@
 import random
 from aprec.datasets.movielens1m import get_genre_dict
 
-from aprec.evaluation.metrics.entropy import Entropy
-from aprec.evaluation.metrics.highest_score import HighestScore
 from aprec.evaluation.metrics.ild import ILD
-from aprec.evaluation.metrics.model_confidence import Confidence
 from aprec.evaluation.metrics.ndcg import NDCG
-from aprec.evaluation.metrics.mrr import MRR
-from aprec.evaluation.metrics.map import MAP
 from aprec.evaluation.metrics.hit import HIT
-from aprec.evaluation.metrics.non_zero_scores import NonZeroScores
 from aprec.evaluation.split_actions import LeaveOneOut
 from aprec.recommenders.filter_seen_recommender import FilterSeenRecommender
-from aprec.recommenders.lightfm import LightFMRecommender
+from aprec.recommenders.fmc_plus import SmartMC
 from aprec.recommenders.sequential.models.generative.reward_metrics.ild_reward import ILDReward
 from aprec.recommenders.sequential.models.generative.reward_metrics.ndcg_reward import NDCGReward
 from aprec.recommenders.sequential.models.generative.reward_metrics.weighted_sum_reward import WeightedSumReward
-from aprec.recommenders.top_recommender import TopRecommender
 
 
 USERS_FRACTIONS = [1.0]
 
-EXTRA_VAL_METRICS = [NDCG(10), HighestScore(), NDCG(40), HIT(1), MRR(), 
-                     Confidence('Softmax'),
-                     Confidence('Sigmoid'),
-                     Entropy('Softmax', 10), 
-                     NonZeroScores(),
-                     HIT(10)]
 
 METRICS = [HIT(1), HIT(10), NDCG(10), ILD(get_genre_dict()) ]
 #TARGET_ITEMS_SAMPLER = PopTargetItemsWithReplacementSampler(101)
@@ -49,7 +36,6 @@ def sasrec_style_model(model_config, sequence_splitter,
                                 sequence_splitter=sequence_splitter, 
                                 targets_builder=target_builder, 
                                 use_keras_training=True,
-                                extra_val_metrics=EXTRA_VAL_METRICS, 
                                 sequence_length=SEQUENCE_LENGTH
                                 )
     
@@ -61,13 +47,12 @@ def generative_tuning_recommender(ild_lambda):
         from aprec.recommenders.sequential.sequential_recommender_config import SequentialRecommenderConfig
         from aprec.recommenders.sequential.target_builders.dummy_builder import DummyTargetBuilder
         from aprec.recommenders.sequential.targetsplitters.id_splitter import IdSplitter
-        from aprec.recommenders.sequential.models.sasrec.sasrec import SASRecConfig
 
 
         model_config = RLGPT2RecConfig(transformer_blocks=3, embedding_size=256, tokenizer='id', tokens_per_item=1, values_per_dim=3500, attention_heads=4)
-        pre_training_recommender = lambda: FilterSeenRecommender(LightFMRecommender(256))
+        pre_training_recommender = lambda: FilterSeenRecommender(SmartMC(order=50, discount=0.6))
 
-        recommender_config = SequentialRecommenderConfig(model_config, train_epochs=200, early_stop_epochs=200,
+        recommender_config = SequentialRecommenderConfig(model_config, train_epochs=10000, early_stop_epochs=200,
                                                batch_size=128,
                                                training_time_limit=200000,  
                                                sequence_splitter=IdSplitter, 
@@ -78,11 +63,13 @@ def generative_tuning_recommender(ild_lambda):
                                                validate_on_loss=True
                                                )
         recommender = GenerativeTuningRecommender(recommender_config, pre_training_recommender,
-                                                  validate_every_steps=20, max_tuning_steps=32000, 
+                                                  validate_every_steps=80, max_tuning_steps=16000, 
                                                   tuning_batch_size=16, 
                                                   clip_eps=0.1,
                                                   reward_metric=WeightedSumReward([NDCGReward(10), ILDReward(get_genre_dict())], [1, ild_lambda]),
                                                   tradeoff_monitoring_rewards=[(NDCGReward(10), ILDReward(get_genre_dict()))],
+                                                  gae_gamma=0.1, 
+                                                  gae_lambda=0.1
                                                   )
         return recommender
         
@@ -112,7 +99,7 @@ def get_recommenders(filter_seen: bool):
     return result
 
 DATASET = "ml1m_items_with5users"
-N_VAL_USERS=128
+N_VAL_USERS=512
 MAX_TEST_USERS=6040
 SPLIT_STRATEGY = LeaveOneOut(MAX_TEST_USERS)
 RECOMMENDERS = get_recommenders(filter_seen=True)
@@ -120,4 +107,3 @@ RECOMMENDERS = get_recommenders(filter_seen=True)
 if __name__ == "__main__":
     from aprec.tests.misc.test_configs import TestConfigs
     TestConfigs().validate_config(__file__)
-
