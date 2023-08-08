6dee2acf04cd22f86069626d2b65c65cb495e872
Aleksandr Petrov <firexel@gmail.com>
2023-08-08 19:50:35 +0100

    evaluate an cleanup checkopint scripts

---
+++ evaluation/cleanup_non_val_checkopints.py
@@ -0,0 +1,32 @@
+import os 
+import sys
+from pathlib import Path
+import time 
+import shutil
+checkpoints_dir = Path(sys.argv[1])
+print(f"monitoring checkpoints in {checkpoints_dir}")
+
+while True:
+    val_checkpoint_steps = []
+    with open(checkpoints_dir/"validations.csv") as f:
+        val_checkpoint_steps = list([int(line.split(',')[0].split('/')[-1].split("_")[-1]) for line in f.readlines()])
+
+    val_checkpoint_steps.sort()   
+    if len(val_checkpoint_steps) == 0:
+        print("no val checkpoints found")
+        exit(0)
+
+    print(val_checkpoint_steps)
+
+    for fname in os.listdir(checkpoints_dir):
+        if fname.startswith("checkpoint_step_"):
+            step = int(fname.split("_")[-1])
+            if step not in val_checkpoint_steps and step < val_checkpoint_steps[-1]:
+                checkpoint_path = checkpoints_dir / fname 
+                print(f"deleting {checkpoint_path}")
+                shutil.rmtree(checkpoint_path)
+                #delete
+                pass
+            else:
+                print(f"keeping step {step}")
+    time.sleep(1)
---
+++ evaluation/configs/generative_rl/ml1m_evaluate_checkpoint.py
@@ -0,0 +1,92 @@
+import json
+import random
+from aprec.datasets.movielens1m import get_genre_dict
+
+from aprec.evaluation.metrics.ild import ILD
+from aprec.evaluation.metrics.ndcg import NDCG
+from aprec.evaluation.metrics.hit import HIT
+from aprec.evaluation.split_actions import LeaveOneOut
+from aprec.recommenders.filter_seen_recommender import FilterSeenRecommender
+from aprec.recommenders.first_order_mc import FirstOrderMarkovChainRecommender
+from aprec.recommenders.fmc_plus import SmartMC
+from aprec.recommenders.lightfm import LightFMRecommender
+from aprec.recommenders.random_recommender import RandomRecommender
+from aprec.recommenders.rl_generative.pre_train_target_splitter import PreTrainTargetSplitter
+from aprec.recommenders.sequential.models.generative.reward_metrics.acc_reward import ACCReward
+from aprec.recommenders.sequential.models.generative.reward_metrics.ild_reward import ILDReward
+from aprec.recommenders.sequential.models.generative.reward_metrics.ndcg_reward import NDCGReward
+from aprec.recommenders.sequential.models.generative.reward_metrics.weighted_sum_reward import WeightedSumReward
+from aprec.recommenders.top_recommender import TopRecommender
+
+
+USERS_FRACTIONS = [1.0]
+
+#checkpoints will be created in any case
+SAVE_MODELS=False
+
+genre_func = get_genre_dict
+
+
+METRICS = [HIT(1), HIT(10), NDCG(10), ILD(genre_func()) ]
+#TARGET_ITEMS_SAMPLER = PopTargetItemsWithReplacementSampler(101)
+
+SEQUENCE_LENGTH=200
+
+#1.0  for gae_gamma and gae_lambda allows the model to see and plan for the whole sequence
+def generative_tuning_recommender(ild_lambda=0.5, checkpoint_dir=CHECKPOINT, gae_gamma=1.0, gae_lambda=1.0, max_tuning_steps=32000):       
+        from aprec.recommenders.rl_generative.generative_tuning_recommender import GenerativeTuningRecommender
+        from aprec.recommenders.sequential.models.generative.gpt_rec_rl import RLGPT2RecConfig
+        from aprec.recommenders.sequential.sequential_recommender_config import SequentialRecommenderConfig
+        from aprec.recommenders.sequential.models.generative.reward_metrics.ild_reward import ILDReward
+        from aprec.recommenders.sequential.models.generative.reward_metrics.ndcg_reward import NDCGReward
+        from aprec.recommenders.sequential.models.generative.reward_metrics.weighted_sum_reward import WeightedSumReward
+
+        model_config = RLGPT2RecConfig(transformer_blocks=3, embedding_size=256, tokenizer='id', tokens_per_item=1, values_per_dim=3500, attention_heads=4)
+        recommender_config = SequentialRecommenderConfig(model_config) 
+        recommender = GenerativeTuningRecommender(recommender_config,
+                                                  pre_trained_checkpoint_dir=checkpoint_dir,
+                                                  max_tuning_steps=32000, 
+                                                  tuning_batch_size=128, 
+                                                  clip_eps=0.1,
+                                                  reward_metric=WeightedSumReward([NDCGReward(10), ILDReward(genre_func())], [1, ild_lambda]),
+                                                  tradeoff_monitoring_rewards=[(NDCGReward(10), ILDReward(genre_func()))],
+                                                  gae_gamma=gae_gamma, 
+                                                  gae_lambda=gae_lambda,
+                                                  validate_before_tuning=True,
+                                                  sampling_processessess=8
+                                                  )
+        with open(CHECKPOINT + "/data_stats.json") as f:
+            data_stats = json.load(f)        
+        recommender.tune = lambda: None
+        recommender.set_val_users(data_stats['val_users'])
+        recommender.rebuild_model()
+        return recommender
+
+recommenders = {
+} 
+
+
+CHECKPOINT="/home/alekspet/Projects/aprec/aprec/evaluation/results/ml1m_items_with5users/ml1_generative_long_tuning_2023_08_08T16_05_30/checkpoints/checkpoint_step_30080"
+recommenders[f"generative_tuning"] = lambda: generative_tuning_recommender(checkpoint_dir = CHECKPOINT)
+    
+
+def get_recommenders(filter_seen: bool):
+    result = {}
+    all_recommenders = list(recommenders.keys())
+    for recommender_name in all_recommenders:
+        if filter_seen:
+            result[recommender_name] =\
+                lambda recommender_name=recommender_name: FilterSeenRecommender(recommenders[recommender_name]())
+        else:
+            result[recommender_name] = recommenders[recommender_name]
+    return result
+
+DATASET = "ml1m_items_with5users"
+N_VAL_USERS=512
+MAX_TEST_USERS=6040
+SPLIT_STRATEGY = LeaveOneOut(MAX_TEST_USERS)
+RECOMMENDERS = get_recommenders(filter_seen=True)
+
+if __name__ == "__main__":
+    from aprec.tests.misc.test_configs import TestConfigs
+    TestConfigs().validate_config(__file__)
