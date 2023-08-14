1695885cf38a5dd5ddb7a94d52141f2cca143fe5
Aleksandr Petrov <firexel@gmail.com>
2023-08-14 14:52:38 +0100

    fixed ild checkpoint script

--- evaluation/configs/generative_rl/ml1m_checkpoint_tuning_ild.py
+++ evaluation/configs/generative_rl/ml1m_checkpoint_tuning_ild.py
@@ -1,20 +1,14 @@
 import random
-from aprec.datasets.movielens1m import get_genre_dict
 
+from aprec.datasets.bert4rec_datasets import get_movielens1m_genres
 from aprec.evaluation.metrics.ild import ILD
 from aprec.evaluation.metrics.ndcg import NDCG
 from aprec.evaluation.metrics.hit import HIT
 from aprec.evaluation.split_actions import LeaveOneOut
 from aprec.recommenders.filter_seen_recommender import FilterSeenRecommender
-from aprec.recommenders.first_order_mc import FirstOrderMarkovChainRecommender
-from aprec.recommenders.fmc_plus import SmartMC
-from aprec.recommenders.lightfm import LightFMRecommender
-from aprec.recommenders.random_recommender import RandomRecommender
-from aprec.recommenders.rl_generative.pre_train_target_splitter import PreTrainTargetSplitter
 from aprec.recommenders.sequential.models.generative.reward_metrics.ild_reward import ILDReward
 from aprec.recommenders.sequential.models.generative.reward_metrics.ndcg_reward import NDCGReward
 from aprec.recommenders.sequential.models.generative.reward_metrics.weighted_sum_reward import WeightedSumReward
-from aprec.recommenders.top_recommender import TopRecommender
 
 
 USERS_FRACTIONS = [1.0]
@@ -22,34 +16,41 @@ USERS_FRACTIONS = [1.0]
 #checkpoints will be created in any case
 SAVE_MODELS=False
 
-genre_func = get_genre_dict
+genre_func = get_movielens1m_genres
 
 
 METRICS = [HIT(1), HIT(10), NDCG(10), ILD(genre_func()) ]
 #TARGET_ITEMS_SAMPLER = PopTargetItemsWithReplacementSampler(101)
 
 SEQUENCE_LENGTH=200
-CHECKPOINT="/home/aprec/Projects/aprec/evaluation/results/checkpoints_for_rl/ml1m_supervised_pre_trained_checkpoint"
+CHECKPOINT="./results/BERT4rec.ml-1m/ml1_generative_long_tuning_2023_08_13T07_36_23/checkpoints/checkpoint_step_18820"
 
-def generative_tuning_recommender(ild_lambda=0.5, checkpoint_dir=CHECKPOINT, gae_gamma=0.1, gae_lambda=0.1):       
+def generative_tuning_recommender(ild_lambda=0.5, checkpoint_dir=CHECKPOINT, gae_gamma=1.0, gae_lambda=1.0, max_tuning_steps=16000):       
         from aprec.recommenders.rl_generative.generative_tuning_recommender import GenerativeTuningRecommender
         from aprec.recommenders.sequential.models.generative.gpt_rec_rl import RLGPT2RecConfig
         from aprec.recommenders.sequential.sequential_recommender_config import SequentialRecommenderConfig
 
 
         model_config = RLGPT2RecConfig(transformer_blocks=3, embedding_size=256, tokenizer='id', tokens_per_item=1, values_per_dim=3500, attention_heads=4)
-        recommender_config = SequentialRecommenderConfig(model_config) 
+        recommender_config = SequentialRecommenderConfig(model_config, train_epochs=0) 
         recommender = GenerativeTuningRecommender(recommender_config,
                                                   pre_trained_checkpoint_dir=checkpoint_dir,
-                                                  max_tuning_steps=32000, 
+                                                  #pre_train_recommender_factory=lambda: RandomRecommender(),
+                                                  max_tuning_steps=max_tuning_steps, 
                                                   tuning_batch_size=128, 
-                                                  clip_eps=0.1,
+                                                  clip_eps=1.0, #allow large changes here, most divergence is controlled by klpen
                                                   reward_metric=WeightedSumReward([NDCGReward(10), ILDReward(genre_func())], [1, ild_lambda]),
                                                   tradeoff_monitoring_rewards=[(NDCGReward(10), ILDReward(genre_func()))],
                                                   gae_gamma=gae_gamma, 
                                                   gae_lambda=gae_lambda,
                                                   validate_before_tuning=True,
-                                                  sampling_processessess=8
+                                                  sampling_processessess=8,
+                                                  batch_recommendation_processess=1,
+                                                  entropy_bonus=0.0,
+                                                  klpen_d_target=0.08,
+                                                  ppo_lr=1e-4,
+                                                  use_klpen=True,
+                                                  supervised_guidance=False
                                                   )
         return recommender
         
