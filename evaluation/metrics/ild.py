93eee24f031d814fd1777b8430a2354ea01ecc69
Aleksandr Petrov <firexel@gmail.com>
2023-08-18 06:55:18 +0100

    set default ild cutoff to 10

--- evaluation/metrics/ild.py
+++ evaluation/metrics/ild.py
@@ -4,13 +4,14 @@ from aprec.recommenders.sequential.models.generative.reward_metrics.ild_reward i
 
 
 class ILD(Metric):
-    def __init__(self, categories_dict):
-        self.name = "ILD"
+    def __init__(self,categories_dict, k=10):
+        self.name = f"ILD@{k}"
         self.less_is_better = False
         self.ild_reward_metric = ILDReward(categories_dict)
+        self.k = k
 
     def __call__(self, recommendations, actual):
-        ild_reward = self.ild_reward_metric(recommendations, actual)
+        ild_reward = self.ild_reward_metric(recommendations[:self.k], actual)
         return float(np.sum(ild_reward))
 
         
