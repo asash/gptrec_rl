1748be0b95152531d734acdd56651b38b148416e
Aleksandr Petrov <firexel@gmail.com>
2023-08-18 08:15:47 +0100

    re-enable keras training

--- evaluation/configs/generative_rl/ml1m_evaluate_checkpoint.py
+++ evaluation/configs/generative_rl/ml1m_evaluate_checkpoint.py
@@ -101,7 +101,7 @@ def sasrec_style_model(model_config, sequence_splitter,
                                 max_batches_per_epoch=256,
                                 sequence_splitter=sequence_splitter, 
                                 targets_builder=target_builder, 
-                                use_keras_training=False,
+                                use_keras_training=True,
                                 sequence_length=SEQUENCE_LENGTH
                                 )
     
