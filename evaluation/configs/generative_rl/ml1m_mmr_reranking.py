863e8e1b4191d8c4026af6dc541602d9495c99cd
Aleksandr Petrov <firexel@gmail.com>
2023-08-19 18:37:07 +0100

    BERT4Rec-mmr experiment with different cutoffs

--- evaluation/configs/generative_rl/ml1m_mmr_reranking.py
+++ evaluation/configs/generative_rl/ml1m_mmr_reranking.py
@@ -16,10 +16,22 @@ SAVE_MODELS=False
 
 genre_func = get_movielens1m_genres
 METRICS = [HIT(1), HIT(10), NDCG(10), ILD(genre_func()), PCOUNT(10, DatasetsRegister()[DATASET]()) ]
-BPR_CHECKPOINT = "./results/BERT4rec.ml-1m/ml1m_evaluate_checkpoint_2023_08_19T10_11_19/checkpoints/mf_bpr.dill.gz"
+
+genres = genre_func()
+BERT4REC_CHECKPOINT = "/mnt/data/experiment_results/BERT4rec.ml-1m/ml1m_evaluate_checkpoint_2023_08_19T13_40_12/checkpoints/bert4rec.dill.gz"
 
 RECOMMENDERS={
-    "MF-BPR-MMR-1.0": lambda: MMRRecommender(BPR_CHECKPOINT, genre_func(), MMR_CUTOFF, 1.0),
+    "BERT4Rec-MMR-0.0": lambda: MMRRecommender(BERT4REC_CHECKPOINT, genres,  MMR_CUTOFF, 0.0),
+    "BERT4Rec-MMR-0.1": lambda: MMRRecommender(BERT4REC_CHECKPOINT, genres,  MMR_CUTOFF, 0.1),
+    "BERT4Rec-MMR-0.2": lambda: MMRRecommender(BERT4REC_CHECKPOINT, genres,  MMR_CUTOFF, 0.2),
+    "BERT4Rec-MMR-0.3": lambda: MMRRecommender(BERT4REC_CHECKPOINT, genres,  MMR_CUTOFF, 0.3),
+    "BERT4Rec-MMR-0.4": lambda: MMRRecommender(BERT4REC_CHECKPOINT, genres,  MMR_CUTOFF, 0.4),
+    "BERT4Rec-MMR-0.5": lambda: MMRRecommender(BERT4REC_CHECKPOINT, genres,  MMR_CUTOFF, 0.5),
+    "BERT4Rec-MMR-0.6": lambda: MMRRecommender(BERT4REC_CHECKPOINT, genres,  MMR_CUTOFF, 0.6),
+    "BERT4Rec-MMR-0.7": lambda: MMRRecommender(BERT4REC_CHECKPOINT, genres,  MMR_CUTOFF, 0.7),
+    "BERT4Rec-MMR-0.8": lambda: MMRRecommender(BERT4REC_CHECKPOINT, genres,  MMR_CUTOFF, 0.8),
+    "BERT4Rec-MMR-0.9": lambda: MMRRecommender(BERT4REC_CHECKPOINT, genres,  MMR_CUTOFF, 0.9),
+    "BERT4Rec-MMR-1.0": lambda: MMRRecommender(BERT4REC_CHECKPOINT, genres,  MMR_CUTOFF, 1.0),
 }
 
 N_VAL_USERS=512
