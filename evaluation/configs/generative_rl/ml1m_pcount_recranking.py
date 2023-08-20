e72a12442e5de72fe89f65cff449177207f05c20
Aleksandr Petrov <firexel@gmail.com>
2023-08-20 13:46:08 +0100

    pass actions instead of genres in popularity reranking module

--- evaluation/configs/generative_rl/ml1m_pcount_recranking.py
+++ evaluation/configs/generative_rl/ml1m_pcount_recranking.py
@@ -18,21 +18,20 @@ genre_func = get_movielens1m_genres
 METRICS = [HIT(1), HIT(10), NDCG(10), ILD(genre_func()), PCOUNT(10, DatasetsRegister()[DATASET]()) ]
 actions = DatasetsRegister()[DATASET]()
 
-genres = genre_func()
 BERT4REC_CHECKPOINT = "/home/aprec/Projects/aprec/evaluation/results/checkpoints_for_rl/ml1m_baselines/bert4rec.dill.gz"
 
 RECOMMENDERS={
-    "BERT4Rec-PCOUNT-0.0": lambda: PCountRecommender(BERT4REC_CHECKPOINT, genres,  PCOUNT_CUTOFF, 0.0),
-    "BERT4Rec-PCOUNT-0.1": lambda: PCountRecommender(BERT4REC_CHECKPOINT, genres,  PCOUNT_CUTOFF, 0.1),
-    "BERT4Rec-PCOUNT-0.2": lambda: PCountRecommender(BERT4REC_CHECKPOINT, genres,  PCOUNT_CUTOFF, 0.2),
-    "BERT4Rec-PCOUNT-0.3": lambda: PCountRecommender(BERT4REC_CHECKPOINT, genres,  PCOUNT_CUTOFF, 0.3),
-    "BERT4Rec-PCOUNT-0.4": lambda: PCountRecommender(BERT4REC_CHECKPOINT, genres,  PCOUNT_CUTOFF, 0.4),
-    "BERT4Rec-PCOUNT-0.5": lambda: PCountRecommender(BERT4REC_CHECKPOINT, genres,  PCOUNT_CUTOFF, 0.5),
-    "BERT4Rec-PCOUNT-0.6": lambda: PCountRecommender(BERT4REC_CHECKPOINT, genres,  PCOUNT_CUTOFF, 0.6),
-    "BERT4Rec-PCOUNT-0.7": lambda: PCountRecommender(BERT4REC_CHECKPOINT, genres,  PCOUNT_CUTOFF, 0.7),
-    "BERT4Rec-PCOUNT-0.8": lambda: PCountRecommender(BERT4REC_CHECKPOINT, genres,  PCOUNT_CUTOFF, 0.8),
-    "BERT4Rec-PCOUNT-0.9": lambda: PCountRecommender(BERT4REC_CHECKPOINT, genres,  PCOUNT_CUTOFF, 0.9),
-    "BERT4Rec-PCOUNT-1.0": lambda: PCountRecommender(BERT4REC_CHECKPOINT, genres,  PCOUNT_CUTOFF, 1.0),
+    "BERT4Rec-PCOUNT-0.0": lambda: PCountRecommender(BERT4REC_CHECKPOINT, actions,  PCOUNT_CUTOFF, 0.0),
+    "BERT4Rec-PCOUNT-0.1": lambda: PCountRecommender(BERT4REC_CHECKPOINT, actions,  PCOUNT_CUTOFF, 0.1),
+    "BERT4Rec-PCOUNT-0.2": lambda: PCountRecommender(BERT4REC_CHECKPOINT, actions,  PCOUNT_CUTOFF, 0.2),
+    "BERT4Rec-PCOUNT-0.3": lambda: PCountRecommender(BERT4REC_CHECKPOINT, actions,  PCOUNT_CUTOFF, 0.3),
+    "BERT4Rec-PCOUNT-0.4": lambda: PCountRecommender(BERT4REC_CHECKPOINT, actions,  PCOUNT_CUTOFF, 0.4),
+    "BERT4Rec-PCOUNT-0.5": lambda: PCountRecommender(BERT4REC_CHECKPOINT, actions,  PCOUNT_CUTOFF, 0.5),
+    "BERT4Rec-PCOUNT-0.6": lambda: PCountRecommender(BERT4REC_CHECKPOINT, actions,  PCOUNT_CUTOFF, 0.6),
+    "BERT4Rec-PCOUNT-0.7": lambda: PCountRecommender(BERT4REC_CHECKPOINT, actions,  PCOUNT_CUTOFF, 0.7),
+    "BERT4Rec-PCOUNT-0.8": lambda: PCountRecommender(BERT4REC_CHECKPOINT, actions,  PCOUNT_CUTOFF, 0.8),
+    "BERT4Rec-PCOUNT-0.9": lambda: PCountRecommender(BERT4REC_CHECKPOINT, actions,  PCOUNT_CUTOFF, 0.9),
+    "BERT4Rec-PCOUNT-1.0": lambda: PCountRecommender(BERT4REC_CHECKPOINT, actions,  PCOUNT_CUTOFF, 1.0),
 }
 
 N_VAL_USERS=512
