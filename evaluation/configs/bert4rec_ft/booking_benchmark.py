42ce0d1b64d21e4c625f104fdee4f7bc114d3519
Aleksandr Petrov <firexel@gmail.com>
2023-10-24 15:54:45 +0100

    deleted extra files

--- evaluation/configs/bert4rec_ft/booking_benchmark.py
+++
@@ -1,8 +0,0 @@
-from aprec.evaluation.split_actions import LeaveOneOut
-from aprec.evaluation.configs.bert4rec_ft.common_benchmark_config import *
-
-DATASET = "booking_warm5"
-N_VAL_USERS=4096
-MAX_TEST_USERS=8196
-SPLIT_STRATEGY = LeaveOneOut(MAX_TEST_USERS)
-RECOMMENDERS = get_recommenders(filter_seen=False)
